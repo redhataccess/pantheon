da479917074fa747a438ed254dab405cb84a2039
Carlos Munoz <camunoz@redhat.com>
2018-10-11 12:04:27 -0400

    Add servlets to display in a different format

---
+++ src/main/java/test/ByResourceTypeServlet.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package test;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+
+//import org.apache.felix.scr.annotations.Properties;
+//import org.apache.felix.scr.annotations.Property;
+//import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello World Servlet registered by resource type
+ *
+ * Annotations below are short version of:
+ * 
+ * @Component
+ * @Service(Servlet.class)
+ * @Properties({
+ *    @Property(name="service.description", value="Hello World Type Servlet"),
+ *    @Property(name="service.vendor", value="The Apache Software Foundation"),
+ *    @Property(name="sling.servlet.resourceTypes", value="sling/servlet/default"),
+ *    @Property(name="sling.servlet.selectors", value="hello"),
+ *    @Property(name="sling.servlet.extensions", value="html")
+ * })
+ */
+@SlingServlet(resourceTypes="pantheon/modules", extensions="adoc")
+@Properties({
+    @Property(name="service.description", value="Hello World Type Servlet"),
+    @Property(name="service.vendor", value="The Apache Software Foundation")
+})
+@SuppressWarnings("serial")
+public class ByResourceTypeServlet extends SlingSafeMethodsServlet {
+    
+    private final Logger log = LoggerFactory.getLogger(ByResourceTypeServlet.class);
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request,
+            SlingHttpServletResponse response) throws ServletException,
+            IOException {
+        Resource resource = request.getResource();
+
+        String content = resource.getValueMap().get("jcr:content", String.class);
+
+        response.setContentType("text/html");
+        Writer w = response.getWriter();
+        w.write(content);
+        
+        log.info("Hello World Servlet");
+        
+    }
+
+}
+
