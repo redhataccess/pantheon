0a067eaa9a63231db806950d92e1a0936964b8a8
Carlos Munoz <camunoz@redhat.com>
2019-02-07 20:46:27 -0500

    add an extension which fetches includes form JCR
    This extension will only work with the latest 1.7.x branch of
    asciidoctorj

---
+++ src/main/java/com/redhat/pantheon/asciidoctor/extension/SlingResourceIncludeProcessor.java
@@ -0,0 +1,39 @@
+package com.redhat.pantheon.asciidoctor.extension;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.asciidoctor.ast.Document;
+import org.asciidoctor.extension.IncludeProcessor;
+import org.asciidoctor.extension.PreprocessorReader;
+
+import java.util.Map;
+
+public class SlingResourceIncludeProcessor extends IncludeProcessor {
+
+    private final ResourceResolver resolver;
+    private final Resource resource;
+
+    public SlingResourceIncludeProcessor(ResourceResolver resolver, Resource resource) {
+        this.resolver = resolver;
+        this.resource = resource;
+    }
+
+    @Override
+    public boolean handles(String target) {
+        return true;
+    }
+
+    @Override
+    public void process(Document document, PreprocessorReader reader, String target, Map<String, Object> attributes) {
+
+        // Find the included file relative to the current resource's location
+        Resource includeResource = resolver.getResource(this.resource.getParent(), target);
+        String content = "Invalid include: " + target;
+
+        if(includeResource != null) {
+            content = includeResource.getChild("jcr:content").getValueMap().get("jcr:data", String.class);
+        }
+
+        reader.push_include(content, target, target, 1, attributes);
+    }
+}
