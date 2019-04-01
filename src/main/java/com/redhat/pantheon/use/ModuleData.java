37fc0317dc7f032e40d0aa4e7f5181d209641235
Ben Radey <benr@redhat.com>
2019-04-01 14:41:06 -0400

    Got dates to display using DateFormatter

--- src/main/java/com/redhat/pantheon/use/ModuleData.java
+++ src/main/java/com/redhat/pantheon/use/ModuleData.java
@@ -3,12 +3,15 @@ package com.redhat.pantheon.use;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.scripting.sightly.pojo.Use;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.query.Query;
 import javax.script.Bindings;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ben on 3/5/19.
@@ -17,6 +20,8 @@ public class ModuleData implements Use {
 
     private ResourceResolver resolver;
     private Resource currentResource;
+
+    private final Logger log = LoggerFactory.getLogger(ModuleData.class);
 
     @Override
     public void init(Bindings bindings) {
@@ -39,6 +44,15 @@ public class ModuleData implements Use {
                 querySuffix, Query.JCR_SQL2);
         List<Resource> ret = new ArrayList<>();
         resources.forEachRemaining(ret::add);
+
+        for (Resource r : ret) {
+            log.trace(r.getName());
+            for (Map.Entry<String, Object> e : r.getValueMap().entrySet()) {
+                log.trace("{} :: {} :: {}", e.getKey(), e.getValue().getClass(), e.getValue());
+            }
+            log.trace("");
+        }
+
         return ret;
     }
 }
