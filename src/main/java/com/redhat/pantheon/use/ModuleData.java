30fd33d2c5d5029f694a9790f5b91e2192785611
Ben Radey <benr@redhat.com>
2019-04-15 15:15:04 -0400

    Tweaking frontend to show content from /repositories

--- src/main/java/com/redhat/pantheon/use/ModuleData.java
+++ src/main/java/com/redhat/pantheon/use/ModuleData.java
@@ -1,5 +1,6 @@
 package com.redhat.pantheon.use;
 
+import com.google.common.collect.Lists;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.scripting.sightly.pojo.Use;
@@ -9,14 +10,19 @@ import org.slf4j.LoggerFactory;
 import javax.jcr.query.Query;
 import javax.script.Bindings;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by ben on 3/5/19.
  */
 public class ModuleData implements Use {
+
+    private static final int PATH_SUBSTRING_INDEX = "/content/".length();
 
     private ResourceResolver resolver;
     private Resource currentResource;
@@ -29,25 +35,39 @@ public class ModuleData implements Use {
         currentResource = (Resource) bindings.get("resource");
     }
 
-    public List<Resource> getModulesNameSort() {
+    public List<Map<String, Object>> getModulesNameSort() {
         return getModules("order by a.[jcr:title] asc");
     }
 
-    public List<Resource> getModulesCreateSort() {
+    public List<Map<String, Object>> getModulesCreateSort() {
         return getModules("order by a.[jcr:created] desc");
     }
 
-    private List<Resource> getModules(String querySuffix) {
+    private List<Map<String, Object>> getModules(String querySuffix) {
         Iterator<Resource> resources = resolver.findResources("select * from [pant:module] as a " +
                 "where [sling:resourceType] = 'pantheon/modules' " +
-                "and isdescendantnode(a, '/content/modules') " +
+                "and (isdescendantnode(a, '/content/repositories') " +
+                    "or isdescendantnode(a, '/content/modules') " +
+                    "or isdescendantnode(a, '/content/sandboxes')) " +
                 querySuffix, Query.JCR_SQL2);
-        List<Resource> ret = new ArrayList<>();
-        resources.forEachRemaining(ret::add);
 
-        for (Resource r : ret) {
+        List<Map<String, Object>> ret = new ArrayList<>();
+
+        while (resources.hasNext()) {
+            Resource r = resources.next();
+            Map<String, Object> m = new HashMap(r.getValueMap());
+            ret.add(m);
             log.trace(r.getName());
-            for (Map.Entry<String, Object> e : r.getValueMap().entrySet()) {
+            m.put("name", r.getName());
+            m.put("pant:transientPath", r.getPath().substring(PATH_SUBSTRING_INDEX));
+            // Example path: /content/repositories/ben_2019-04-11_16-15-15/shared/attributes.module.adoc
+            String[] fragments = r.getPath().split("/");
+            // Example fragments: ["", "content", "repositories", "ben_2019-04-11_16-15-15", "shared", "attributes.module.adoc"]
+            m.put("pant:transientSource", fragments[2]);
+            if (!"modules".equals(fragments[2])) {
+                m.put("pant:transientSourceName", fragments[3]);
+            }
+            for (Map.Entry<String, Object> e : m.entrySet()) {
                 log.trace("{} :: {} :: {}", e.getKey(), e.getValue().getClass(), e.getValue());
             }
             log.trace("");
