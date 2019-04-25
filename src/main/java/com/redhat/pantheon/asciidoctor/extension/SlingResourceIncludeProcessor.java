e5f94e8788e26f2b58d9405906f827f1a4d28631
Ben Radey <benr@redhat.com>
2019-04-25 15:00:11 -0400

    Adding logging

--- src/main/java/com/redhat/pantheon/asciidoctor/extension/SlingResourceIncludeProcessor.java
+++ src/main/java/com/redhat/pantheon/asciidoctor/extension/SlingResourceIncludeProcessor.java
@@ -5,10 +5,14 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.asciidoctor.ast.Document;
 import org.asciidoctor.extension.IncludeProcessor;
 import org.asciidoctor.extension.PreprocessorReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
 public class SlingResourceIncludeProcessor extends IncludeProcessor {
+
+    private final Logger log = LoggerFactory.getLogger(SlingResourceIncludeProcessor.class);
 
     private ResourceResolver resolver;
     private Resource resource;
@@ -25,14 +29,19 @@ public class SlingResourceIncludeProcessor extends IncludeProcessor {
 
     @Override
     public void process(Document document, PreprocessorReader reader, String target, Map<String, Object> attributes) {
+        log.trace("Attempting to include {}", target);
+        log.trace("Resource: {}", resource);
 
         // Find the included file relative to the current resource's location
         Resource parent = resource.getParent();
+        log.trace("Resource parent: {}", parent);
         Resource includeResource = resolver.getResource(parent, target);
         // Odds are good that our fist attempt was looking for something like "include.adoc", but our resource was
         // simply named "include", so try again after dropping the suffix.
         if (includeResource == null && target.contains(".")) {
-            includeResource = resolver.getResource(parent, target.substring(0, target.lastIndexOf('.')));
+            String newTarget = target.substring(0, target.lastIndexOf('.'));
+            log.trace("Could not find {}, searching for {}", target, newTarget);
+            includeResource = resolver.getResource(parent, newTarget);
         }
         String content = "Invalid include: " + target;
 
@@ -50,6 +59,8 @@ public class SlingResourceIncludeProcessor extends IncludeProcessor {
                     .getValueMap()
                     .get("jcr:data", String.class);
             }
+        } else {
+            log.warn("Could not find include for {}", target);
         }
 
         reader.push_include(content, target, target, 1, attributes);
