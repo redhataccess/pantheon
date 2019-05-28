5a5be057b95d6f1958ab18e209d2058b853cb635
Joseph Marrero <jmarrero@users.noreply.github.com>
2019-05-28 15:52:10 -0400

    Update Brand with login logic (#53)

--- frontend/src/app/components/Chrome/Header/Brand/Brand.tsx
+++ frontend/src/app/components/Chrome/Header/Brand/Brand.tsx
@@ -1,3 +1,38 @@
-import * as React from 'react';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
-export const Brand: React.FunctionComponent<{}> = () => <h1>Pantheon</h1>;
+export default class Brand extends Component {
+    public state = {
+        isLoggedIn: false,
+        linkText: 'Pantheon | Log In'
+    };
+
+    public render() {
+        if (!this.state.isLoggedIn) {
+            fetch("/system/sling/info.sessionInfo.json")
+                .then(response => response.json())
+                .then(responseJSON => {
+                    if (responseJSON['userID'] !== 'anonymous') {
+                        this.setState({ linkText: 'Pantheon | Log Out [' + responseJSON['userID'] + ']' })
+                        this.setState({ isLoggedIn: true })
+                    }
+                })
+        }
+
+        return (
+            <React.Fragment>
+                <Link to={this.state.isLoggedIn ? '/logout' : '/login'}
+                    onClick={this.conditionalRedirect}>
+                    {this.state.linkText}
+                </Link>
+            </React.Fragment>
+        );
+    }
+
+    private conditionalRedirect = () => {
+        if (this.state.linkText.includes("Log Out")) {
+            fetch('/system/sling/logout')
+                .then(response => window.location.href = "/pantheon")
+        }
+    }
+}
