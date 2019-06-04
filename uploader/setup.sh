5e0e56ead7b32517533caafafb350b97c46ee386
Lisa Davidson <xdavidson@users.noreply.github.com>
2019-06-04 16:52:20 -0400

    setup script to install pantheon.py (#63)

---
+++ uploader/setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+# Variables
+UPLOADER_HOME=~/.pantheon
+PANTHEON_BIN=/usr/local/bin/pantheon
+
+# Create UPLOADER_HOME directory if not exists
+if [ ! -d "$UPLOADER_HOME" ]; then
+  echo "Creating UPLOADER_HOME directory: $UPLOADER_HOME"
+  mkdir -p $UPLOADER_HOME
+fi
+
+# Download the uploader script to $UPLOADER_HOME 
+echo "Downloading the uploader script to $UPLOADER_HOME"
+curl -o ${UPLOADER_HOME}/pantheon.py https://raw.githubusercontent.com/redhataccess/pantheon/master/uploader/pantheon.py
+
+# Ensure file is executable
+echo "Ensure uploader script is executble"
+chmod +x ${UPLOADER_HOME}/pantheon.py 
+
+# Create a symlink to /usr/local/bin if not exists
+if [ ! -h "$PANTHEON_BIN" ]; then
+  echo "Create a symlink to $PANTHEON_BIN"
+  sudo ln -s ${UPLOADER_HOME}/pantheon.py $PANTHEON_BIN
+fi
+
+# Show full path for pantheon command
+echo
+echo "UPLOADER_HOME: $UPLOADER_HOME"
+echo "pantheon command path: "
+which pantheon
+
+if [[ $(which pantheon) = $PANTHEON_BIN ]]; then
+  echo "set up completed!"
+fi  
+echo
