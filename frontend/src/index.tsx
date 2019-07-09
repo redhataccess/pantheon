4cb06f4f07b39bf9088aef99d2e29f159f1165aa
Joseph Marrero <jmarrero@users.noreply.github.com>
2019-07-09 14:20:30 -0400

    Use hashRouter and remove catch all 404 servlet (#82)
    * Use hashRouter and remove catch all 404 servlet

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "@patternfly/react-core/dist/styles/base.css";
 import { App } from '@app/app';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 if (process.env.NODE_ENV !== "production") {
   // tslint:disable-next-line
