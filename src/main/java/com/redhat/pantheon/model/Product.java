3fc0b6a2336a4ff7d8c608963d3a41abcd103056
Lisa Davidson <llu@redhat.com>
2019-07-22 14:59:01 -0400

    [CCS-2961] code review fixes

--- src/main/java/com/redhat/pantheon/model/Product.java
+++ src/main/java/com/redhat/pantheon/model/Product.java
@@ -9,21 +9,47 @@ import org.apache.sling.models.annotations.Default;
 
 import com.redhat.pantheon.model.api.SlingResource;
 
+/**
+ * Represents a product as a sling resource.
+ * A product has many versions.
+ */
 public class Product extends SlingResource {
 
+	/**
+	 * Represents the product's slingResourceType.
+	 */
     @Default(values = "pantheon/product")
     public final Field<String> slingResourceType = new Field<>(String.class, "sling:resourceType");
 
+    /**
+     * Represents the product's created date.
+     */
     public final Field<Calendar> created = new Field<>(Calendar.class, "jcr:created");
 
+    /**
+     * Represents the user who created the product.
+     */
     public final Field<String> createdBy = new Field<>(String.class, "jcr:createdBy");
 
+    /**
+     * Represents the product's primaryType.
+     */
     public final Field<String> primaryType = new Field<>(String.class, "jcr:primaryType");
 
+    /**
+     * Represents the product's name.
+     */
     public final Field<String> name = new Field<>(String.class, "name");
     
-    public final Field<String> productDescription = new Field<>(String.class, "productDescription");
+    /**
+     * Represents the product's description.
+     */
+    public final Field<String> description = new Field<>(String.class, "description");
     
+    /**
+     * Creates a new Product with the given resource object.
+     * @param resource
+     */
     public Product(@Nonnull Resource resource) {
         super(resource);
     }
--- src/main/java/com/redhat/pantheon/model/ProductVersion.java
+++ src/main/java/com/redhat/pantheon/model/ProductVersion.java
@@ -9,19 +9,42 @@ import org.apache.sling.models.annotations.Default;
 
 import com.redhat.pantheon.model.api.SlingResource;
 
+/**
+ * Represents a ProductVersion as a sling resource.
+ * A productVersion is associated with a product.
+ */
 public class ProductVersion extends SlingResource {
 
+	/**
+	 * Represents the productVersion's slingResourceType.
+	 */
     @Default(values = "pantheon/productVersion")
     public final Field<String> slingResourceType = new Field<>(String.class, "sling:resourceType");
 
+    /**
+     * Represents the productVersion's created date.
+     */
     public final Field<Calendar> created = new Field<>(Calendar.class, "jcr:created");
 
+    /**
+     * Represents the user who created the productVersion.
+     */
     public final Field<String> createdBy = new Field<>(String.class, "jcr:createdBy");
 
+    /**
+     * Represents the productVersion's primaryType.
+     */
     public final Field<String> primaryType = new Field<>(String.class, "jcr:primaryType");
 
+    /**
+     * Represents the productVersion's name.
+     */
     public final Field<String> name = new Field<>(String.class, "name");
     
+    /**
+     * Creates a new ProductVersion with given resource.
+     * @param resource
+     */
     public ProductVersion(@Nonnull Resource resource) {
         super(resource);
     }
