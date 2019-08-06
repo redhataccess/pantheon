eeb591a17c89855618616d28c946b3d2a9d5c9d4
Joseph Marrero <jmarrero@users.noreply.github.com>
2019-08-06 13:38:29 -0400

    Add Dockerfile and yaml files to build on openshift. (#100)
    * Add Dockerfile and update Readme.

---
+++ container/containerInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true
+do
+  STATUS=$(curl --max-time 4 -s -o /dev/null -w '%{http_code}' http://localhost:8080/is_alive)
+  if [ $STATUS -eq 404 ]; then
+    echo "Got 404! Sling should be ready!"
+    mv /pantheon-1.0-SNAPSHOT.jar /install/
+    break
+  else
+    echo "Got $STATUS :( Not done yet..."
+  fi
+  sleep 10
+done
