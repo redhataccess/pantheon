591cd2032d1b91f3aa65d8b15b61a6135b9d430d
Joseph Marrero <jmarrero@redhat.com>
2019-08-14 13:59:01 -0400

    change mv to cp

--- container/containerInstall.sh
+++ container/containerInstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 while true
 do
-  STATUS=$(curl --max-time 4 -s -o /dev/null -w '%{http_code}' http://0.0.0.0:8080/is_alive)
+  STATUS=$(curl --max-time 4 -s -o /dev/null -w '%{http_code}' http://localhost:8080/is_alive)
   if [ $STATUS -eq 404 ]; then
     echo "Got 404! Sling should be ready!"
-    mv /pantheon-1.0-SNAPSHOT.jar /install/
+    cp /pantheon-1.0-SNAPSHOT.jar /install/
     break
   else
     echo "Got $STATUS :( Not done yet..."
