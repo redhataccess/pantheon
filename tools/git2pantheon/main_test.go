578dbae8f725d1348e941ce8a4da3c5252579301
Joseph Marrero <jmarrero@redhat.com>
2019-08-23 10:44:48 -0400

    Check for variable in go

--- tools/git2pantheon/main_test.go
+++ tools/git2pantheon/main_test.go
@@ -31,3 +31,27 @@ func TestCloneBranch(t *testing.T) {
 		t.Errorf("The request did not included a valid Repository but the system think it does.")
 	}
 }
+
+func TestGetInfo(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://example.com/info", nil)
+	w := httptest.NewRecorder()
+	getInfo(w, req)
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	if !strings.Contains(string(body), "not set") {
+		t.Errorf("ENV OPENSHIFT_BUILD_COMMIT should always be not set during build.")
+	}
+
+	req = httptest.NewRequest("POST", "http://example.com/info", nil)
+	w = httptest.NewRecorder()
+	getInfo(w, req)
+	resp = w.Result()
+	body, _ = ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+
+	if !strings.Contains(string(body), "Invalid request method") {
+		t.Errorf("This method should only work with a GET. It worked with a POST.")
+	}
+}
