f29b4c9c7d6e7163b0faf4f0d3874162db0d1702
Lisa Davidson <llu@redhat.com>
2019-09-04 15:39:09 -0400

    [CCS-2986] inital work on Edit Metadata Modal

--- frontend/src/app/moduleDisplay.tsx
+++ frontend/src/app/moduleDisplay.tsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Button } from '@patternfly/react-core';
+import {
+    BaseSizes, Button, Dropdown, DropdownToggle, DropdownItem, DropdownSeparator, Form,
+    FormGroup, Modal, Title, TitleLevel, TextInput
+} from '@patternfly/react-core';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbHeading } from '@patternfly/react-core';
 import { Link } from "react-router-dom";
 import { Level, LevelItem } from '@patternfly/react-core';
@@ -7,21 +10,74 @@ import { Tooltip } from '@patternfly/react-core';
 import {
     DataList, DataListItem, DataListItemRow, DataListItemCells,
     DataListCell, Card, Text, TextContent, TextVariants, Grid, GridItem
-  } from '@patternfly/react-core';
+} from '@patternfly/react-core';
 import { Revisions } from '@app/revisions';
-import { HelpIcon } from '@patternfly/react-icons';
+import { HelpIcon, ThIcon, CaretDownIcon } from '@patternfly/react-icons';
 
 export interface IProps {
     moduleName: string
     modulePath: string
     moduleType: string
-    updated: string    
-  }
-  
-  class ModuleDisplay extends Component<IProps> {
+    updated: string
+}
+
+class ModuleDisplay extends Component<IProps> {
+    public state = {
+        formInvalid: false,
+        isDropdownOpen: false,
+        isDup: false,
+        isModalOpen: false,
+        productUrl: ''
+    };
 
     public render() {
-        return (  
+        const { isDropdownOpen, isModalOpen, productUrl } = this.state;
+        const header = (
+            <React.Fragment>
+                <Title headingLevel={TitleLevel.h1} size={BaseSizes["2xl"]}>
+                    Edit Metadata
+              </Title>
+                <p className="pf-u-pl-sm">
+                    All fields are required.
+              </p>
+            </React.Fragment>
+        );
+        const productDropdownItems = [
+            <DropdownItem key="link">Link</DropdownItem>,
+            <DropdownItem key="action" component="button">
+                Action
+            </DropdownItem>,
+            <DropdownSeparator key="separator" />,
+            <DropdownItem key="separated link">Separated Link</DropdownItem>,
+            <DropdownItem key="separated action" component="button">
+                Separated Action
+            </DropdownItem>
+        ];
+
+        const versionDropdownItems = [
+            <DropdownItem key="link">1.1</DropdownItem>,
+            <DropdownItem key="action" component="button">
+                1.2
+            </DropdownItem>,
+            <DropdownSeparator key="separator" />,
+            <DropdownItem key="separated link">Separated Link</DropdownItem>,
+            <DropdownItem key="separated action" component="button">
+                Separated Action
+            </DropdownItem>
+        ];
+
+        const documentUseCaseItems = [
+            <DropdownItem key="link">Case Study</DropdownItem>,
+            <DropdownItem key="action" component="button">
+                Documentation
+            </DropdownItem>,
+            <DropdownSeparator key="separator" />,
+            <DropdownItem key="separated link">Separated Link</DropdownItem>,
+            <DropdownItem key="separated action" component="button">
+                Separated Action
+            </DropdownItem>
+        ];
+        return (
             <React.Fragment>
                 <div>
                     <Breadcrumb>
@@ -34,23 +90,23 @@ export interface IProps {
                 <div>
                     <Level gutter="md">
                         <LevelItem>
-                                <TextContent>
-                                    <Text component={TextVariants.h1}>{this.props.moduleName}{'  '}
+                            <TextContent>
+                                <Text component={TextVariants.h1}>{this.props.moduleName}{'  '}
                                     <Tooltip
-                                                position="right"
-                                                content={
-                                                    <div>Title updated in latest revision</div>
-                                                }>
-                                            <span><HelpIcon/></span>
-                                        </Tooltip>
-                                    </Text>  
-                                </TextContent>
+                                        position="right"
+                                        content={
+                                            <div>Title updated in latest revision</div>
+                                        }>
+                                        <span><HelpIcon /></span>
+                                    </Tooltip>
+                                </Text>
+                            </TextContent>
                         </LevelItem>
                         <LevelItem />
                         <LevelItem>
-                            <Button variant="secondary">Edit metadata</Button>
+                            <Button variant="secondary" onClick={this.handleModalToggle}>Edit metadata</Button>
                         </LevelItem>
-                    </Level>                
+                    </Level>
                 </div>
                 <div>
                     <a href='http://access.redhat.com'>View on Customer Portal</a>
@@ -62,7 +118,7 @@ export interface IProps {
                                 <DataListItemCells
                                     dataListCells={[
                                         <DataListCell width={2} key="products">
-                                            <span className="sp-prop-nosort" id="span-source-type">Products</span>                                            
+                                            <span className="sp-prop-nosort" id="span-source-type">Products</span>
                                         </DataListCell>,
                                         <DataListCell key="published">
                                             <span className="sp-prop-nosort" id="span-source-type">Published</span>
@@ -97,21 +153,104 @@ export interface IProps {
                             </DataListItemRow>
                             ))}
                     </DataListItem>
-                </DataList>
+                    </DataList>
                 </div>
                 <div>
                     <Card>
-                        <Revisions 
+                        <Revisions
                             modulePath={this.props.modulePath}
                             revisionModulePath={this.props.moduleName}
                         />
                     </Card>
                 </div>
+                <Modal
+                    width={'50%'}
+                    title="Edit metadata"
+                    isOpen={isModalOpen}
+                    header={header}
+                    ariaDescribedById="edit-metadata"
+                    onClose={this.handleModalToggle}
+                    actions={[
+                        <Button key="confirm" variant="primary" onClick={this.handleModalToggle}>
+                            Save
+          </Button>,
+                        <Button key="cancel" variant="secondary" onClick={this.handleModalToggle}>
+                            Cancel
+            </Button>
+                    ]}
+                >
+                    <Form isHorizontal={true}>
+                        <FormGroup
+                            label="Product Name"
+                            isRequired={true}
+                            fieldId="product-name"
+                        >
+                            <Dropdown
+                                onSelect={this.onSelect}
+                                toggle={<DropdownToggle onToggle={this.onToggle} iconComponent={CaretDownIcon}>Dropdown</DropdownToggle>}
+                                isOpen={isDropdownOpen}
+                                dropdownItems={productDropdownItems}
+                            />
+                            <Dropdown
+                                onSelect={this.onSelect}
+                                toggle={<DropdownToggle onToggle={this.onToggle} iconComponent={CaretDownIcon}>Dropdown</DropdownToggle>}
+                                isOpen={isDropdownOpen}
+                                dropdownItems={versionDropdownItems}
+                            />
+                        </FormGroup>
+                        <FormGroup
+                            label="Document Usecase"
+                            isRequired={true}
+                            fieldId="document-usecase"
+                        >
+                            <Dropdown
+                                onSelect={this.onSelect}
+                                toggle={<DropdownToggle onToggle={this.onToggle} iconComponent={CaretDownIcon}>Dropdown</DropdownToggle>}
+                                isOpen={isDropdownOpen}
+                                dropdownItems={documentUseCaseItems}
+                            />
+                        </FormGroup>
+                        <FormGroup
+                            label="Vanity URL Fragment"
+                            isRequired={true}
+                            fieldId="url-fragment"
+                        >
+                            <div><span>/</span><TextInput isRequired={true} id="url-fragment" type="text" placeholder="URL Fragment" value={productUrl} onChange={this.handleURLInput} /></div>
+                        </FormGroup>
+                    </Form>
+                </Modal>
             </React.Fragment>
 
         );
     }
-    
+
+
+    private handleModalToggle = () => {
+        this.setState({
+            isModalOpen: !this.state.isModalOpen
+        });
+    }
+    private onSelect = event => {
+        this.setState({
+            isDropdownOpen: !this.state.isDropdownOpen
+        });
+    }
+
+    private onToggle = isDropdownOpen => {
+        this.setState({
+            isDropdownOpen
+        });
+    }
+
+    private handleURLInput = productUrl => {
+        this.setState({ productUrl });
+
+        // check for duplcated product URL.
+        // this.productUrlExist(this.state.productUrl);
+        if (this.state.isDup) {
+            this.setState({ formInvalid: true });
+        }
+    }
 }
 
 export { ModuleDisplay }
