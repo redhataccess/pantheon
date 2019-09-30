44618606667acf89f159d36af25fbd48e1aca11c
Ben Radey <benr@redhat.com>
2019-09-30 13:33:25 -0400

    Adding javadoc and tweaking dereference map construction

--- src/main/java/com/redhat/pantheon/servlet/GetObjectByUUID.java
+++ src/main/java/com/redhat/pantheon/servlet/GetObjectByUUID.java
@@ -70,7 +70,7 @@ public class GetObjectByUUID extends SlingSafeMethodsServlet {
         Map<String, Set<String>> dereferenceMap = new HashMap<>();
         if (!isNullOrEmpty(dereference)) {
             try {
-                buildDereferenceMap(dereference, dereferenceMap);
+                dereferenceMap.putAll(buildDereferenceMap(dereference));
             } catch (Exception e) {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter '" + PARAM_DEREFERENCE + "' must follow the pattern resourceTypeA:propertyA,resourceTypeB:propertyB,etc...");
             }
@@ -88,7 +88,21 @@ public class GetObjectByUUID extends SlingSafeMethodsServlet {
         }
     }
 
-    private void buildDereferenceMap(String dereference, Map<String, Set<String>> map) {
+    /**
+     * Accepts incoming URL parameter "dereference" as a String and builds a Map of properties to dereference, based
+     * off of the sling resource type that the property is found on. For example:
+     * Input: "pantheon/moduleLocale:draft,pantheon/moduleLocale:released,pantheon/moduleVersion:product"
+     * Output:
+     *  {
+     *      "pantheon/moduleLocale": ["draft","released"],
+     *      "pantheon/moduleVersion": ["product"]
+     *  }
+     * @param dereference The incoming dereference specification
+     * @return A Map of Sets containing the sling resource type as the Key, and the Set of properties to dereference
+     *  for that resource type as the Value.
+     */
+    private Map<String, Set<String>> buildDereferenceMap(String dereference) {
+        Map<String, Set<String>> map = new HashMap<>();
         for (String token : dereference.split(",")) {
             String[] pair = token.split(":", 2);
             Set<String> props = map.get(pair[0]);
@@ -98,6 +112,7 @@ public class GetObjectByUUID extends SlingSafeMethodsServlet {
             }
             props.add(pair[1]);
         }
+        return map;
     }
 
     private Resource getResourceByUuid(String uuid) throws RepositoryException {
