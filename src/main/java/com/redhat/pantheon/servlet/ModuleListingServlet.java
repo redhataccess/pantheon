ac417d3188bc80a59d69187c8700a2be2f2664b1
ankit.n24 <ananavat@redhat.com>
2019-10-11 11:32:49 -0400

    Removing duplicate modules

--- src/main/java/com/redhat/pantheon/servlet/ModuleListingServlet.java
+++ src/main/java/com/redhat/pantheon/servlet/ModuleListingServlet.java
@@ -52,14 +52,14 @@ public class ModuleListingServlet extends AbstractJsonQueryServlet {
         // is not returning any results
         StringBuilder queryBuilder = new StringBuilder()
                 .append("select m.* from [nt:base] as m ")
-                    .append("INNER JOIN [pant:moduleVersion] as rev ON ISDESCENDANTNODE(rev, m) ")
+                    .append("INNER JOIN [pant:moduleLocale] as rev ON ISDESCENDANTNODE(rev, m) ")
                 .append("where (isdescendantnode(m, '/content/repositories') ")
                     .append("or isdescendantnode(m, '/content/modules') ")
                     .append("or isdescendantnode(m, '/content/sandbox')) ")
-                .append("AND m.[jcr:primaryType] = 'pant:module' ")
+                .append("AND m.[jcr:primaryType] = 'pant:module' ");
                 // look in ALL versions (all locales)
-                .append("AND (rev.[metadata/jcr:title] like '%" + searchParam + "%' ")
-                    .append("OR rev.[metadata/jcr:description] like " + "'%" + searchParam + "%') ");
+                // .append("AND (rev.[metadata/jcr:title] like '%" + searchParam + "%' ")
+                //     .append("OR rev.[metadata/jcr:description] like " + "'%" + searchParam + "%') ");
 
         if(!isNullOrEmpty(keyParam) && !isNullOrEmpty(directionParam)) {
             queryBuilder.append(" order by rev.[metadata/")
