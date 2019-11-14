2c9381f856125df1781dd0bc6ba5f82fcd11ca25
Carlos Munoz <camunoz@redhat.com>
2019-11-14 12:32:00 -0500

    simplify the event handling mechanism (#169)
    This change simplifies the event handling mechanism.
    
    All events should now go through a single topic for processing. Each extension is responsible for filtering out the events for which they care.
    
    All that is needed to create an extension point is to extend the EventProcessingExtension interface which has two methods: one to filter out all events that the extension doesn't care about, and another one to do the actual processing. The created class should be annotated with @Component(service = EventProcessingExtension.class)
    
    The simplest filter can be by event type, as there are different types of events being fired.

--- pantheon-bundle/src/main/java/com/redhat/pantheon/servlet/module/ReleaseDraftVersion.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/servlet/module/ReleaseDraftVersion.java
@@ -2,6 +2,7 @@ package com.redhat.pantheon.servlet.module;
 
 import com.redhat.pantheon.conf.GlobalConfig;
 import com.redhat.pantheon.extension.Events;
+import com.redhat.pantheon.extension.events.ModuleVersionPublishedEvent;
 import com.redhat.pantheon.model.module.Module;
 import com.redhat.pantheon.model.module.ModuleLocale;
 import com.redhat.pantheon.model.module.ModuleVersion;
@@ -63,7 +64,7 @@ public class ReleaseDraftVersion extends AbstractPostOperation {
             changes.add(Modification.onModified(module.getPath()));
 
             // call the extension point
-            events.fireModuleVersionPublishedEvent(moduleLocale.released.getReference());
+            events.fireEvent(new ModuleVersionPublishedEvent(moduleLocale.released.getReference().getPath()));
         }
     }
 }
