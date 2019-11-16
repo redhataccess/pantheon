41a17888c1a97feecadf16be59602a5eeaf15289
Lisa Davidson <llu@redhat.com>
2019-11-15 20:08:34 -0500

    [CCS-2972] add annotation

--- pantheon-bundle/src/main/java/com/redhat/pantheon/extension/ModulePostPublishHydraIntegration.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/extension/ModulePostPublishHydraIntegration.java
@@ -1,27 +1,39 @@
 package com.redhat.pantheon.extension;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Locale;
+import java.util.Optional;
 
 import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.net.ssl.SSLContext;
 
 import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.ResourceUtil;
 import org.fusesource.stomp.jms.StompJmsConnectionFactory;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import com.redhat.pantheon.extension.events.ModuleVersionPublishedEvent;
+import com.redhat.pantheon.model.module.Content;
 import com.redhat.pantheon.model.module.Module;
+import com.redhat.pantheon.model.module.ModuleVersion;
+import com.redhat.pantheon.sling.ServiceResourceResolverProvider;
 
 /**
  * A Hydra message producer for Module post publish events.
  * 
  */
+@Component(
+        service = ModulePostPublishHydraIntegration.class
+        
+)
 public class ModulePostPublishHydraIntegration implements EventProcessingExtension {
 	// Environment variables.
 	private static String message_broker_hostname = "";
@@ -35,12 +47,25 @@ public class ModulePostPublishHydraIntegration implements EventProcessingExtensi
 	private static final String TLS_VERSION = "TLSv1.2";
 	private static final String UUID_FIELD = "jcr:uuid";
 	private static final String HYDRA_TOPIC = "VirtualTopic.eng.pantheon2.notifications";
+	public static final Locale DEFAULT_MODULE_LOCALE = Locale.US;
 	private SSLContext sslContext;
-	private ResourceResolverFactory resolverFactory;
+	private ServiceResourceResolverProvider serviceResourceResolverProvider;
+	private final Logger log = LoggerFactory.getLogger(ModulePostPublishHydraIntegration.class);
 
+
+	@Activate
+	public ModulePostPublishHydraIntegration(
+			@Reference ServiceResourceResolverProvider serviceResourceResolverProvider) {
+		this.serviceResourceResolverProvider = serviceResourceResolverProvider;
+	}
+	
 	public boolean canProcessEvent(Event event) {
-		//TODO: return true if resource is published
-		return true;
+		// Return true if resource is published
+		ModuleVersionPublishedEvent publishedEvent = (ModuleVersionPublishedEvent) event;
+		Resource resource = serviceResourceResolverProvider.getServiceResourceResolver().getResource(ResourceUtil.getParent(publishedEvent.getModuleVersionPath(), 2));
+		Module module = resource.adaptTo(Module.class);
+		
+		return module.getReleasedContent(DEFAULT_MODULE_LOCALE).isPresent();
 	}
 	
 	public void processEvent(Event event) throws Exception {
@@ -50,49 +75,26 @@ public class ModulePostPublishHydraIntegration implements EventProcessingExtensi
     		Resource resource = null;
     		Module module = null;
     
-    		// Get resourceResolver
-    		Map<String, Object> serviceParams = new HashMap<String, Object>();
-    
-            serviceParams.put(ResourceResolverFactory.SUBSERVICE, "pantheon");
-    
             try {
-            
-            	// get resource resolver for the system user set in user mapper
-                ResourceResolver resourceResolver = resolverFactory.getServiceResourceResolver(serviceParams);
-
-                // get resource from path
-                resource = resourceResolver.getResource(null, ResourceUtil.getParent(publishedEvent.getModuleVersionPath(), 2));
+            	// Get resource from path
+                resource = serviceResourceResolverProvider.getServiceResourceResolver().getResource(ResourceUtil.getParent(publishedEvent.getModuleVersionPath(), 2));
                 module = resource.adaptTo(Module.class);
             } catch (Exception e) {
             	System.out.println(e.getMessage());
             }
-    		
-    		byte[] byteArray = Base64.decodeBase64(this.getMesasgeBrokerUserPass().getBytes());
-    		String decodedPass = new String(byteArray);
-    		sslContext = SSLContext.getInstance(TLS_VERSION);
-    		
-    		sslContext.init(null, null, new java.security.SecureRandom());
-    		 
-            StompJmsConnectionFactory factory = new StompJmsConnectionFactory();
-            factory.setBrokerURI(this.getMessageBrokerScheme() + "://" + this.getPanthoenHost() +":" + this.getMessageBrokerPort());
-    
-            factory.setUsername(this.getMesasgeBrokerUsername());
-            factory.setPassword(decodedPass);
-            factory.setSslContext(sslContext);
-            factory.setForceAsyncSend(true);
-            factory.setDisconnectTimeout(5000);
             
-            Connection connection = factory.createConnection();
+            Connection connection = createConnectionFactory().createConnection();
             try {
             	
     			connection.start(); 
-    			  
+    			log.info("[ModulePostPublishHydraIntegration] connection started " );  
     		    Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+    		    log.info("[ModulePostPublishHydraIntegration] createSession " );
     		    MessageProducer producer = session.createProducer(session.createTopic(HYDRA_TOPIC));
     		    String moduleUUID = module.getValueMap().get(UUID_FIELD, String.class);
     		    String msg = "{\"id\": " + this.getPanthoenHost() + PANTHEON_MODULE_API_PATH + moduleUUID +"}";
     			producer.send(session.createTextMessage(msg));
-     
+    			log.info("[ModulePostPublishHydraIntegration] message sent: " + session.createTextMessage(msg) );
     		} catch (Throwable t) {
     			t.printStackTrace();
     		} finally {
@@ -166,4 +168,23 @@ public class ModulePostPublishHydraIntegration implements EventProcessingExtensi
 
 		return pantheon_host;
 	}
+	
+	private ConnectionFactory createConnectionFactory() throws Exception {
+		byte[] byteArray = Base64.decodeBase64(this.getMesasgeBrokerUserPass().getBytes());
+		String decodedPass = new String(byteArray);
+		sslContext = SSLContext.getInstance(TLS_VERSION);
+		
+		sslContext.init(null, null, new java.security.SecureRandom());
+		 
+        StompJmsConnectionFactory factory = new StompJmsConnectionFactory();
+        factory.setBrokerURI(this.getMessageBrokerScheme() + "://" + this.getPanthoenHost() +":" + this.getMessageBrokerPort());
+
+        factory.setUsername(this.getMesasgeBrokerUsername());
+        factory.setPassword(decodedPass);
+        factory.setSslContext(sslContext);
+        factory.setForceAsyncSend(true);
+        factory.setDisconnectTimeout(5000);
+        
+        return factory;
+    }
 }
