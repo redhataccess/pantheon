e4802b3d0f16fc7550755ce73cbe66b6753dc220
Joseph Marrero <jmarrero@redhat.com>
2019-12-02 14:34:52 -0500

    add test

--- pantheon-bundle/frontend/src/app/Dropdown.test.tsx
+++ pantheon-bundle/frontend/src/app/Dropdown.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Dropdown } from '@app/Dropdown'
 import '@app/fetchMock'
-
 import { mount, shallow } from 'enzyme'
+import sinon from 'sinon'
+import renderer from 'react-test-renderer'
 
 describe('Tests for Dropdown', () => {
 
@@ -21,6 +22,21 @@ describe('Tests for Dropdown', () => {
         newPerPagevalue={"25 items per page"}
       />
     )
+  })
+
+  it('test changeSelection function', () => {
+    const wrapper = renderer.create(<Dropdown
+      perPageValue={move}
+      newPerPagevalue={"25 items per page"}
+    />)
+    const inst = wrapper.getInstance()
+    const spy = sinon.spy(inst, 'changeSelection')
+    inst.changeSelection(wrapper)
+    sinon.assert.called(spy)
+
+    const spy2 = sinon.spy(inst, 'changeExpandState')
+    inst.changeExpandState()
+    sinon.assert.called(spy2)
   })
 
   function move(){
