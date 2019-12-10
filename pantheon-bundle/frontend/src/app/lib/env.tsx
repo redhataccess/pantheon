90ef3ee7769e98fee18d04be27e32dd495dcd329
Lisa Davidson <llu@redhat.com>
2019-12-10 11:39:06 -0500

    remove dotenv packages

--- pantheon-bundle/frontend/src/app/lib/env.tsx
+++
@@ -1,7 +0,0 @@
-import { resolve } from "path"
-
-import { config } from "dotenv"
-
-config({ path: resolve(__dirname, "../../../.env") })
-console.log('[env] process.env.CP_HOST ' + process.env.CP_HOST)
-console.log("[env] process.env.NODE_ENV " + process.env)
