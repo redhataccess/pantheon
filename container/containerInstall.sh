c5d6b69d2cdae7269ba07609839406cda12f6b31
Joseph Marrero <jmarrero@users.noreply.github.com>
2020-03-07 10:29:51 -0500

    Bump pantheon's version

--- container/containerInstall.sh
+++ container/containerInstall.sh
@@ -4,7 +4,7 @@ do
   STATUS=$(curl --max-time 4 -s -o /dev/null -w '%{http_code}' http://localhost:8080/is_alive)
   if [ $STATUS -eq 404 ]; then
     echo "Got 404! Sling should be ready!"
-    cp /pantheon-1.0-SNAPSHOT.jar /install/
+    cp /pantheon-0.1.1.jar /install/
     break
   else
     echo "Got $STATUS :( Not done yet..."
