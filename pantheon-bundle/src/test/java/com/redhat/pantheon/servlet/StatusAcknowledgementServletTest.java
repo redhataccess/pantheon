b8e876a5b86c6bf8c5e9ba02f733a7348bc87848
Lisa Davidson <llu@redhat.com>
2020-04-16 10:57:11 -0400

    WIP servlet test

--- pantheon-bundle/src/test/java/com/redhat/pantheon/servlet/StatusAcknowledgementServletTest.java
+++ pantheon-bundle/src/test/java/com/redhat/pantheon/servlet/StatusAcknowledgementServletTest.java
@@ -4,13 +4,23 @@ import com.redhat.pantheon.model.api.SlingModels;
 import com.redhat.pantheon.model.module.Module;
 import org.apache.commons.lang3.LocaleUtils;
 import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.servlets.HttpConstants;
+
+import org.apache.sling.testing.mock.osgi.MockOsgi;
+import org.apache.sling.testing.mock.sling.MockSling;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.junit5.SlingContext;
 import org.apache.sling.testing.mock.sling.junit5.SlingContextExtension;
+import org.apache.sling.testing.mock.sling.servlet.MockRequestPathInfo;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.osgi.framework.BundleContext;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -22,9 +32,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith({SlingContextExtension.class})
 public class StatusAcknowledgementServletTest {
+    private static final @NotNull BundleContext BundleContext = null;
     SlingContext slingContext = new SlingContext(ResourceResolverType.JCR_OAK);
     String testHTML = "<!DOCTYPE html> <html lang=\"en\"> <head><title>test title</title></head> <body " +
             "class=\"article\"><h1>test title</h1> </header> </body> </html>";
+    // get bundle context
+    BundleContext bundleContext = MockOsgi.newBundleContext();
 
     @BeforeEach
     public void setUp(){
@@ -55,18 +68,31 @@ public class StatusAcknowledgementServletTest {
     }
     @Test
     public void testAddAcknowledgement() throws ServletException, IOException {
-
+        // prepare sling request
+        MockSlingHttpServletRequest request = new MockSlingHttpServletRequest(bundleContext);
+ 
         String resourceUuid = slingContext.resourceResolver()
                 .getResource("/content/repositories/repo/module")
                 .getValueMap()
                 .get("jcr:uuid")
                 .toString();
+        // set current resource
+//        request.setResource(slingContext.resourceResolver().getResource("/content/repositories/repo/module"));
+        // set method
+        request.setMethod(HttpConstants.METHOD_POST);
+        request.setServerName("www.example.com");
+        request.setContentType("application/json");
+        request.setPathInfo("/api/status");
 
         Charset utf8 = Charset.forName("UTF-8");
         String data = "{\"id\":\""+resourceUuid+"\",\"status\": \"received\",\"sender\":\"hydra\",\"message\":\"from hydra\"}";
-        slingContext.request().setContent(data.getBytes(utf8));
+        request.setContent(data.getBytes(utf8));
+        
+        // prepare sling response
+        MockSlingHttpServletResponse response = new MockSlingHttpServletResponse();
+
         StatusAcknowledgeServlet statusAcknowledgeServlet = new StatusAcknowledgeServlet();
-        statusAcknowledgeServlet.doPost(slingContext.request(), slingContext.response());
+        statusAcknowledgeServlet.doPost(request, response);
         Assertions.assertEquals(200, slingContext.response().getStatus(), "Status should be 200");
         Module module =
                 SlingModels.getModel(
