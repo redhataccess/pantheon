8495d53b47d59a51b13817ea0f2ceedfe3d68d33
Carlos Munoz <camunoz@redhat.com>
2020-05-11 14:30:47 -0400

    add a more compact null-safe way to traverse jcr structures using models (#287)
    Add a more compact null-safe way to traverse jcr structures using models.
    This revision deprecates the existing Child.map method and adds a similar but more compact way to do complex JCR structure traversals and avoid null elements in the path.
    
    See the unit tests for the way to use it.

--- pantheon-bundle/src/main/java/com/redhat/pantheon/model/api/Child.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/model/api/Child.java
@@ -40,6 +40,9 @@ public interface Child<T extends SlingModel> extends Supplier<T> {
      * @return An optional indicating the result of the operation. If the operation
      * returns null, or if the value of this child was not present in the first place,
      * this returns an empty Optional
+     * @deprecated Use {@link com.redhat.pantheon.model.api.util.SafeResourceTraversal#start(SlingModel)}
+     * for safe resource traversals
      */
+    @Deprecated
     <R> Optional<R> map(Function<? super T, ? extends R> func);
 }
