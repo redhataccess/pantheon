3c484d850c9474ae1772968d2b2e2282de338c12
rednitish <50713102+rednitish@users.noreply.github.com>
2020-05-23 12:38:30 +0530

    Ccs 3597 modules jcr changes (#292)
    * multi variant upload enabled
    
    * multi variant upload enabled, in case of single variant, canonical is optional

--- uploader/pantheon.py
+++ uploader/pantheon.py
@@ -322,6 +322,8 @@ def process_workspace(path):
                     module_variants['pant:attributesFilePath'] = value
                 if key.lower() == 'canonical':
                     module_variants['pant:canonical'] = value
+            if 'pant:canonical' not in module_variants:
+                module_variants['pant:canonical'] = 'true'
             if 'pant:name' in module_variants:
                 data[module_variants['pant:name']] = module_variants
             createVariant(data, path, url)
@@ -329,7 +331,7 @@ def process_workspace(path):
     else:
         data = {'DEFAULT': {}}
         createVariant(data, path, url)
-        
+
 def createVariant(data, path, url):
     payload = {}
     payload[':content'] = json.dumps(data)  # '{"sample":"test"}'
