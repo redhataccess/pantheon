cbb5a25a808f52db1b283b0288e70dba0aeccb1c
Ben Radey <benjamin.radey+gpg@gmail.com>
2020-06-15 15:24:37 -0400

    Adds ability for symlinks to ignore extra slashes (#311)

--- pantheon-bundle/src/main/java/com/redhat/pantheon/servlet/assets/ImageServletFilter.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/servlet/assets/ImageServletFilter.java
@@ -1,10 +1,9 @@
 package com.redhat.pantheon.servlet.assets;
 
+import com.redhat.pantheon.helper.Symlinks;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.servlets.annotations.SlingServletFilter;
-import org.jetbrains.annotations.NotNull;
 import org.osgi.service.component.annotations.Component;
 
 import javax.servlet.Filter;
@@ -58,27 +57,9 @@ public class ImageServletFilter implements Filter {
         String imagePath = new String(Base64.getUrlDecoder().decode(assetId));
         StringBuilder realPath = new StringBuilder(imagePath);
 
-        //Find out if this resolves by itself already.
-        ResourceResolver resolver = ((SlingHttpServletRequest) request).getResourceResolver();
-        Resource res = resolver.resolve(imagePath);
-        if (res == null || "sling:nonexisting".equals(res.getResourceType())) {
-            //Because of symlinks, we need to resolve imagePath manually.
-            String[] parts = imagePath.split("/");
-            realPath.setLength(0);
-            for (String part : parts) {
-                if (part.isEmpty()) {
-                    continue;
-                }
-                realPath.append("/").append(part);
-                Resource resource = resolver.resolve(realPath.toString());
-                if ("pantheon/symlink".equals(resource.getResourceType())) {
-                    realPath.setLength(realPath.length() - part.length());
-                    realPath.append(resource.getValueMap().get("pant:target", String.class));
-                }
-            }
-        }
+        Resource image = Symlinks.resolve(((SlingHttpServletRequest) request).getResourceResolver(), imagePath);
 
-        request.getRequestDispatcher(realPath.toString())
+        request.getRequestDispatcher(image.getPath())
             .forward(request, response);
     }
 
