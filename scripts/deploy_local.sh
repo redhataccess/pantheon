a04dd5661d496b0d6aa18ed4b853318874e84935
A.P.Rajshekhar <randalap@redhat.com>
2020-07-01 15:11:16 +0530

    CCS-3603 Migrate Pantheon 2 to Apache Karaf (#330)
    * CCS-3609:  Create Karaf distribution of Pantheon.

---
+++ scripts/deploy_local.sh
@@ -0,0 +1,26 @@
+set -o allexport
+
+if [ -z "$PANTHEON_CODEBASE" ]
+then
+      echo "\$PANTHEON_CODEBASE is empty"
+      exit 1
+fi
+
+CONF_FILE=$PANTHEON_CODEBASE/scripts/pantheon_karaf.exports
+
+if [[ -f ~/.pantheon/pantheon_karaf.exports ]]; then
+    CONF_FILE=~/.pantheon/pantheon_karaf.exports
+fi
+
+
+source $CONF_FILE
+set +o allexport
+
+cd $PANTHEON_CODEBASE
+
+set -e
+# build the distribution
+./mvnw clean install  -U -pl pantheon-bundle,pantheon-karaf-feature,pantheon-karaf-dist
+pantheon-karaf-dist/target/assembly/bin/karaf
+set +e
+
