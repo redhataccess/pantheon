65d0b1b5d241b4b5846b82d50cc1a761cf8a653e
NITISH SHARMA <nisharma@redhat.com>
2020-07-29 22:26:47 +0530

    PnT JIRA (CCS-3683) unable to publish module when logged in as a demo user

--- pantheon-bundle/src/main/java/com/redhat/pantheon/servlet/module/PublishDraftVersion.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/servlet/module/PublishDraftVersion.java
@@ -7,20 +7,23 @@ import com.redhat.pantheon.extension.events.ModuleVersionPublishedEvent;
 import com.redhat.pantheon.model.HashableFileResource;
 import com.redhat.pantheon.model.api.FileResource;
 import com.redhat.pantheon.model.module.*;
+import com.redhat.pantheon.sling.ServiceResourceResolverProvider;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.PersistenceException;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.servlets.post.*;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
+import java.util.*;
 
 import static com.redhat.pantheon.jcr.JcrResources.rename;
 import static com.redhat.pantheon.model.api.util.ResourceTraversal.traverseFrom;
@@ -38,12 +41,16 @@ public class PublishDraftVersion extends AbstractPostOperation {
 
     private Events events;
     private AsciidoctorService asciidoctorService;
+    private ServiceResourceResolverProvider serviceResourceResolverProvider;
+    private Logger logger = LoggerFactory.getLogger(PublishDraftVersion.class);
 
     @Activate
     public PublishDraftVersion(@Reference Events events,
-                               @Reference AsciidoctorService asciidoctorService) {
+                               @Reference AsciidoctorService asciidoctorService,
+                               @Reference ServiceResourceResolverProvider serviceResourceResolverProvider) {
         this.events = events;
         this.asciidoctorService = asciidoctorService;
+        this.serviceResourceResolverProvider = serviceResourceResolverProvider;
     }
 
     private Module getModule(SlingHttpServletRequest request) {
@@ -60,6 +67,8 @@ public class PublishDraftVersion extends AbstractPostOperation {
 
     @Override
     public void run(SlingHttpServletRequest request, PostResponse response, SlingPostProcessor[] processors) {
+        logger.debug("Operation Publishinging draft version started");
+        long startTime = System.currentTimeMillis();
         super.run(request, response, processors);
         if (response.getError() == null) {
             // call the extension point
@@ -75,64 +84,74 @@ public class PublishDraftVersion extends AbstractPostOperation {
             asciidoctorService.getModuleHtml(module, locale, variant, false, new HashMap(), true);
             events.fireEvent(new ModuleVersionPublishedEvent(moduleVersion), 15);
         }
+        log.debug("Operation Publishinging draft version,  completed");
+        long elapseTime = System.currentTimeMillis() - startTime;
+        log.info("Total elapsed http request/response time in milliseconds: " + elapseTime);
     }
 
     @Override
-    protected void doRun(SlingHttpServletRequest request, PostResponse response, List<Modification> changes) {
-        Locale locale = getLocale(request);
-        Module module = getModule(request);
-        String variant = getVariant(request);
+    protected void doRun(SlingHttpServletRequest request, PostResponse response, List<Modification> changes) throws  RepositoryException{
+        try (ResourceResolver serviceResourceResolver = serviceResourceResolverProvider.getServiceResourceResolver()) {
+            Module module = serviceResourceResolver.getResource(request.getResource().getPath()).adaptTo(Module.class);
+            Locale locale = getLocale(request);
+            String variant = getVariant(request);
 
-        // Get the draft version, there should be one
-        Optional<ModuleVersion> versionToRelease = module.getDraftVersion(locale, variant);
-        if( !versionToRelease.isPresent() ) {
-            response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED,
-                    "The module doesn't have a draft version to be released");
-        } else if (versionToRelease.get().metadata().getOrCreate().productVersion().get() == null
-        		||  versionToRelease.get().metadata().getOrCreate().productVersion().get().isEmpty()) {
-        	// Check if productVersion is set
-        	response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED,
-                    "The version to be released doesn't have productVersion metadata");
-        } else if (versionToRelease.get().metadata().getOrCreate().urlFragment().get() == null
-        		||  versionToRelease.get().metadata().getOrCreate().urlFragment().get().isEmpty()) {
-        	// Check if urlFragment is set
-        	response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED,
-                    "The version to be released doesn't have urlFragment metadata");
-        } else {
-            // Draft becomes the new released version
-            ModuleVariant moduleVariant = traverseFrom(module)
-                    .toChild(m -> module.locale(locale))
-                    .toChild(ModuleLocale::variants)
-                    .toChild(variants -> variants.variant(variant))
-                    .get();
-            moduleVariant.releaseDraft();
-            changes.add(Modification.onModified(module.getPath()));
-            // source/draft becomes source/released
-            FileResource draftSource = traverseFrom(module)
-                    .toChild(m -> module.locale(locale))
-                    .toChild(ModuleLocale::source)
-                    .toChild(sourceContent -> sourceContent.draft())
-                    .get();
-            // Check for released version
-            Optional<HashableFileResource> releasedSource = traverseFrom(module)
-                    .toChild(m -> module.locale(locale))
-                    .toChild(ModuleLocale::source)
-                    .toChild(sourceContent -> sourceContent.released())
-                    .getAsOptional();
-            if (draftSource != null) {
-                if (releasedSource.isPresent()) {
+            // Get the draft version, there should be one
+            Optional<ModuleVersion> versionToRelease = module.getDraftVersion(locale, variant);
+            if (!versionToRelease.isPresent()) {
+                response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED,
+                        "The module doesn't have a draft version to be released");
+            } else if (versionToRelease.get().metadata().getOrCreate().productVersion().get() == null
+                    || versionToRelease.get().metadata().getOrCreate().productVersion().get().isEmpty()) {
+                // Check if productVersion is set
+                response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED,
+                        "The version to be released doesn't have productVersion metadata");
+            } else if (versionToRelease.get().metadata().getOrCreate().urlFragment().get() == null
+                    || versionToRelease.get().metadata().getOrCreate().urlFragment().get().isEmpty()) {
+                // Check if urlFragment is set
+                response.setStatus(HttpServletResponse.SC_PRECONDITION_FAILED,
+                        "The version to be released doesn't have urlFragment metadata");
+            } else {
+                // Draft becomes the new released version
+                ModuleVariant moduleVariant = traverseFrom(module)
+                        .toChild(m -> module.locale(locale))
+                        .toChild(ModuleLocale::variants)
+                        .toChild(variants -> variants.variant(variant))
+                        .get();
+                moduleVariant.releaseDraft();
+                changes.add(Modification.onModified(module.getPath()));
+                // source/draft becomes source/released
+                FileResource draftSource = traverseFrom(module)
+                        .toChild(m -> module.locale(locale))
+                        .toChild(ModuleLocale::source)
+                        .toChild(sourceContent -> sourceContent.draft())
+                        .get();
+                // Check for released version
+                Optional<HashableFileResource> releasedSource = traverseFrom(module)
+                        .toChild(m -> module.locale(locale))
+                        .toChild(ModuleLocale::source)
+                        .toChild(sourceContent -> sourceContent.released())
+                        .getAsOptional();
+                if (draftSource != null) {
+                    if (releasedSource.isPresent()) {
+                        try {
+                            releasedSource.get().delete();
+                        } catch (PersistenceException e) {
+                            throw new RuntimeException("Failed to remove source/released: " + releasedSource.get().getPath());
+                        }
+                    }
                     try {
-                        releasedSource.get().delete();
-                    } catch (PersistenceException e) {
-                        throw new RuntimeException("Failed to remove source/released: " + releasedSource.get().getPath());
+                        rename(draftSource, "released");
+                    } catch (RepositoryException e) {
+                        throw new RuntimeException("Cannot find source/draft: " + draftSource.getPath());
                     }
                 }
-                try {
-                    rename(draftSource, "released");
-                } catch (RepositoryException e) {
-                    throw new RuntimeException("Cannot find source/draft: " + draftSource.getPath());
+                if(serviceResourceResolver.hasChanges()) {
+                    serviceResourceResolver.commit();
                 }
             }
+        }catch (Exception ex){
+            throw new RepositoryException(ex.getMessage());
         }
     }
 }
