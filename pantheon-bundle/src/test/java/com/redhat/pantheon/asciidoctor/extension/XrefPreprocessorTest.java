30a819108bb026ec02d17e33bab0c8f463a2ef8c
Ben Radey <benjamin.radey+gpg@gmail.com>
2020-08-13 14:27:21 -0400

    Triangle xrefs work

--- pantheon-bundle/src/test/java/com/redhat/pantheon/asciidoctor/extension/XrefPreprocessorTest.java
+++ pantheon-bundle/src/test/java/com/redhat/pantheon/asciidoctor/extension/XrefPreprocessorTest.java
@@ -50,6 +50,10 @@ public class XrefPreprocessorTest {
                         "sling:resourceType", "pantheon/module")
                 .resource("/moduleB/en_US/variants/test-atts",
                         "jcr:uuid", "efgh5678")
+                .resource("/moduleC",
+                        "sling:resourceType", "pantheon/module")
+                .resource("/moduleC/en_US/variants/test-atts",
+                        "jcr:uuid", "ijkl9012")
                 .commit();
         TestUtils.registerMockAdapter(DocumentVariant.class, slingContext);
         TestUtils.registerMockAdapter(Document.class, slingContext);
@@ -58,10 +62,17 @@ public class XrefPreprocessorTest {
 
         //When
         XrefPreprocessor xp = new XrefPreprocessor(variant, new TableOfContents());
-        List<String> output = xp.preprocess(Arrays.asList("xref:moduleB[Link Label]"));
+        List<String> output = xp.preprocess(Arrays.asList(
+                "xref:moduleB[Link Label B1]",
+                "xref:moduleB#deep-target-b[Link Label B2]",
+                "<<moduleC,Link Label C1>>",
+                "<<moduleC#deep-target-c,Link Label C2>>"));
 
         //Then
-        assertEquals("xref:efgh5678#[Link Label]", output.get(0));
+        assertEquals("xref:efgh5678#[Link Label B1]", output.get(0));
+        assertEquals("xref:efgh5678#deep-target-b[Link Label B2]", output.get(1));
+        assertEquals("xref:ijkl9012#[Link Label C1]", output.get(2));
+        assertEquals("xref:ijkl9012#deep-target-c[Link Label C2]", output.get(3));
     }
 
     /**
@@ -88,9 +99,12 @@ public class XrefPreprocessorTest {
 
         //When
         XrefPreprocessor xp = new XrefPreprocessor(variant, toc);
-        List<String> output = xp.preprocess(Arrays.asList("xref:moduleB[Link Label]"));
+        List<String> output = xp.preprocess(Arrays.asList(
+                "xref:moduleB[Link Label B1]",
+                "<<moduleB,Link Label B2>>"));
 
         //Then
-        assertEquals("xref:#_abcd1234[Link Label]", output.get(0));
+        assertEquals("xref:#_abcd1234[Link Label B1]", output.get(0));
+        assertEquals("xref:#_abcd1234[Link Label B2]", output.get(1));
     }
 }
