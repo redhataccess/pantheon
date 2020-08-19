314539c0585de64a05148d3e035844caa06bd9db
Lisa Davidson <llu@redhat.com>
2020-08-19 15:13:34 -0400

    fix versions tests

--- pantheon-bundle/frontend/src/app/versions.test.tsx
+++ pantheon-bundle/frontend/src/app/versions.test.tsx
@@ -3,7 +3,10 @@ import { Versions, IProps } from '@app/versions'
 import '@app/fetchMock'
 
 import { mount, shallow } from 'enzyme'
-import { Button, Card, DataList, DataListItem, DataListItemCells, DataListItemRow, DataListToggle, Dropdown, Form, FormGroup, FormSelect, FormSelectOption, InputGroup, InputGroupText, Modal, TextInput, DropdownItem, Title, Alert, AlertActionCloseButton, DataListContent, KebabToggle } from '@patternfly/react-core'
+import { 
+    Button, Form, FormGroup, FormSelect, FormSelectOption, InputGroup,
+    InputGroupText, Modal, Title, Alert, AlertActionCloseButton, Grid
+} from '@patternfly/react-core'
 import renderer from 'react-test-renderer'
 import sinon from 'sinon'
 import { any } from 'prop-types';
@@ -28,191 +31,114 @@ describe('Versions tests', () => {
         expect(view).toMatchSnapshot()
     })
 
-    // it('should render a Button', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const button = wrapper.find(Button)
-    //     expect(button.exists()).toBe(true)
-    // })
+    it('should render a Modal', () => {
+        const wrapper = mount(<Versions {...props} />)
+        // console.log(wrapper.debug())
+        const modal = wrapper.find(Modal)
+        expect(modal.exists()).toBe(true)
+    })
 
-    // it('should render a Card', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const card = wrapper.find(Card)
-    //     expect(card.exists()).toBe(true)
-    // })
+    it('should render a Grid', () => {
+        const wrapper = mount(<Versions {...props} />)
+        const grid = wrapper.find(Grid)
+        expect(grid.exists()).toBe(true)
+    })
 
-    // it('should render a Data List', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const dataList = wrapper.find(DataList)
-    //     expect(dataList.exists()).toBe(true)
-    // })
+    it('should render a Form', () => {
+        const wrapper = shallow(<Versions {...props} />)
+        wrapper.setState({ 'login': true })
+        wrapper.setState({ 'isModalOpen': true })
+        const form = wrapper.find(Form)
+        expect(form.exists()).toBe(true)
+    })
 
-    // it('should render a DataListItem', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const dataListItem = wrapper.find(DataListItem)
-    //     expect(dataListItem.exists()).toBe(true)
-    // })
+    it('should render a FormGroup', () => {
+        const wrapper = shallow(<Versions {...props} />)
+        wrapper.setState({ 'login': true })
+        wrapper.setState({ 'isModalOpen': true })
+        const formGroup = wrapper.find(FormGroup)
+        expect(formGroup.exists()).toBe(true)
+    })
 
-    // it('should render a DataListItemCells Element', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const dataListItemCells = wrapper.find(DataListItemCells)
-    //     expect(dataListItemCells.exists()).toBe(true)
-    // })
+    it('should render a FormSelect', () => {
+        const wrapper = shallow(<Versions {...props} />)
+        wrapper.setState({ 'login': true })
+        wrapper.setState({ 'isModalOpen': true })
+        const formSelect = wrapper.find(FormSelect)
+        expect(formSelect.exists()).toBe(true)
+    })
 
-    // it('should render a DataListItemRow element', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const dataListItemRow = wrapper.find(DataListItemRow)
-    //     expect(dataListItemRow.exists()).toBe(true)
-    // })
+    it('should render a FormSelectOption', () => {
+        const wrapper = shallow(<Versions {...props} />)
+        wrapper.setState({ 'login': true })
+        wrapper.setState({ 'isModalOpen': true })
+        const formSelectOption = wrapper.find(FormSelectOption)
+        expect(formSelectOption.exists()).toBe(true)
+    })
 
-    // it('should render a DataListToggle', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const dataListToggle = wrapper.find(DataListToggle)
-    //     expect(dataListToggle.exists()).toBe(true)
-    // })
+    it('should render a InputGroup', () => {
+        const wrapper = shallow(<Versions {...props} />)
+        wrapper.setState({ 'login': true })
+        wrapper.setState({ 'isModalOpen': true })
+        const inputGroup = wrapper.find(InputGroup)
+        expect(inputGroup.exists()).toBe(true)
+    })
 
-    // it('should render a DataListContent', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const dataListContent = wrapper.find(DataListContent)
-    //     expect(dataListContent.exists()).toBe(true)
-    // })
+    it('should render a success Alert', () => {
+        const wrapper = shallow(<Versions {...props} />)
+        wrapper.setState({ 'login': true })
+        wrapper.setState({ 'successAlertVisible': true })
+        const alert = wrapper.find(Alert)
+        expect(alert.exists()).toBe(true)
+    })
 
-    // it('should render a Modal', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const modal = wrapper.find(Modal)
-    //     expect(modal.exists()).toBe(true)
-    // })
+    it('test fetchVersions function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.fetchVersions()).toMatchSnapshot()
+    })
 
-    // it('should render a Form', () => {
-    //     const wrapper = shallow(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({ 'isModalOpen': true })
-    //     const form = wrapper.find(Form)
-    //     expect(form.exists()).toBe(true)
-    // })
+    it('test changePublishState function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.changePublishState("Publish")).toMatchSnapshot()
+    })
 
-    // it('should render a FormGroup', () => {
-    //     const wrapper = shallow(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({ 'isModalOpen': true })
-    //     const formGroup = wrapper.find(FormGroup)
-    //     expect(formGroup.exists()).toBe(true)
-    // })
+    it('test previewDoc function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.previewDoc()).toMatchSnapshot()
+    })
 
-    // it('should render a FormSelect', () => {
-    //     const wrapper = shallow(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({ 'isModalOpen': true })
-    //     const formSelect = wrapper.find(FormSelect)
-    //     expect(formSelect.exists()).toBe(true)
-    // })
+    it('test saveMetadata function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.saveMetadata()).toMatchSnapshot()
+    })
 
-    // it('should render a FormSelectOption', () => {
-    //     const wrapper = shallow(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({ 'isModalOpen': true })
-    //     const formSelectOption = wrapper.find(FormSelectOption)
-    //     expect(formSelectOption.exists()).toBe(true)
-    // })
+    it('test onChangeUsecase function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.onChangeUsecase()).toMatchSnapshot()
+    })
 
-    // it('should render a InputGroup', () => {
-    //     const wrapper = shallow(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({ 'isModalOpen': true })
-    //     const inputGroup = wrapper.find(InputGroup)
-    //     expect(inputGroup.exists()).toBe(true)
-    // })
+    it('test handleURLInput function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.handleURLInput()).toMatchSnapshot()
+    })
 
-    // it('should render a success Alert', () => {
-    //     const wrapper = shallow(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({ 'successAlertVisible': true })
-    //     const alert = wrapper.find(Alert)
-    //     expect(alert.exists()).toBe(true)
-    // })
+    it('test loginRedirect function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.loginRedirect()).toMatchSnapshot()
+    })
 
-    // it('should render a KebabToggle element', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({
-    //         'results': [[{ "type": "draft", "icon": "BlankImage", "path": "/modules/test", "version": "Version 1", "publishedState": 'Not published', "updatedDate": "", "firstButtonType": 'primary', "secondButtonType": 'secondary', "firstButtonText": 'Publish', "secondButtonText": 'Preview', "isDropdownOpen": false, "isArchiveDropDownOpen": false, "metadata": '' }]],
-    //     })
-    //     const kebabToggle = wrapper.find(KebabToggle)
-    //     expect(kebabToggle.exists()).toBe(true)
-    // })
-
-    // // it('test fetchVersions function', () => {
-    // //     const wrapper = renderer.create(<Versions {...props} />)
-    // //     const inst = wrapper.getInstance()
-    // //     expect(inst.fetchVersions()).toMatchSnapshot()
-    // // })
-
-    // // it('test changePublishState function', () => {
-    // //     const wrapper = renderer.create(<Versions {...props} />)
-    // //     const inst = wrapper.getInstance()
-    // //     expect(inst.changePublishState("Publish")).toMatchSnapshot()
-    // // })
-
-    // it('test onArchiveSelect function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.onArchiveSelect()).toMatchSnapshot()
-    // })
-
-    // it('test onHeadingToggle function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.onHeadingToggle()).toMatchSnapshot()
-    // })
-
-    // it('test onExpandableToggle function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     const data = [{ "isDropdownOpen": true }]
-    //     expect(inst.onExpandableToggle(data)).toMatchSnapshot()
-    // })
-
-    // it('test onArchiveToggle function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     const data = [{ "isDropdownOpen": true }]
-    //     expect(inst.onArchiveToggle(data)).toMatchSnapshot()
-    // })
-
-    // it('test previewDoc function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.previewDoc()).toMatchSnapshot()
-    // })
-
-    // it('test saveMetadata function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.saveMetadata()).toMatchSnapshot()
-    // })
-
-    // it('test onChangeUsecase function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.onChangeUsecase()).toMatchSnapshot()
-    // })
-
-    // it('test handleURLInput function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.handleURLInput()).toMatchSnapshot()
-    // })
-
-    // it('test loginRedirect function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.loginRedirect()).toMatchSnapshot()
-    // })
-
-    // it('test dismissNotification function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.dismissNotification()).toMatchSnapshot()
-    // })
+    it('test dismissNotification function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.dismissNotification()).toMatchSnapshot()
+    })
 
     it('test getMetadata function', () => {
         const wrapper = shallow(<Versions {...props} />)
@@ -228,43 +154,34 @@ describe('Versions tests', () => {
         expect(useCaseValue.exists()).toBe(true)
     })
 
-    // it('test getHarrayChildNamed function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.getHarrayChildNamed("__children__")).toMatchSnapshot()
-    // })
+    it('test getHarrayChildNamed function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.getHarrayChildNamed("__children__")).toMatchSnapshot()
+    })
 
-    // it('test hideSuccessAlert function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.hideSuccessAlert()).toMatchSnapshot()
-    // })
+    it('test hideSuccessAlert function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.hideSuccessAlert()).toMatchSnapshot()
+    })
 
-    // it('test hidePublishAlert function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />);
-    //     const inst = wrapper.getInstance();
-    //     expect(inst.hidePublishAlert()).toMatchSnapshot();
-    // });
-
-    // Value testing with Enzyme.
-    // it('renders Version heading', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const sourceTypeText = wrapper.find('#span-source-type-version').text()
-
-    //     // ensure it matches what is expected
-    //     expect(sourceTypeText).toEqual("Version")
-    // })
+    it('test hidePublishAlert function', () => {
+        const wrapper = renderer.create(<Versions {...props} />);
+        const inst = wrapper.getInstance();
+        expect(inst.hidePublishAlert()).toMatchSnapshot();
+    });
 
     it('has a props', () => {
         const versions = mount(<Versions {...props} />).matchesElement
         expect(versions.length === 1)
     })
 
-    // it('test getHarrayChildNamed function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     expect(inst.getHarrayChildNamed(anymatch, 'metadata')).toMatchSnapshot()
-    // })
+    it('test getHarrayChildNamed function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        expect(inst.getHarrayChildNamed(anymatch, 'metadata')).toMatchSnapshot()
+    })
 
     it('has a variantUUID of "1234"', () => {
         const state: IProps = {
@@ -283,39 +200,39 @@ describe('Versions tests', () => {
         expect(state.versionModulePath).toEqual('versionPath')
     })
 
-    // test('changePublishState click', () => {
-    //     const wrapper = mount(<Versions {...props} />)
-    //     const instance = wrapper.instance()
-    //     wrapper.setState({ 'login': true })
-    //     wrapper.setState({
-    //         'results': [[{ "type": "draft", "icon": "BlankImage", "path": "/modules/test", "version": "Version 1", "publishedState": 'Not published', "updatedDate": "", "firstButtonType": 'primary', "secondButtonType": 'secondary', "firstButtonText": 'Publish', "secondButtonText": 'Preview', "isDropdownOpen": false, "isArchiveDropDownOpen": false, "metadata": '' }]],
-    //     })
-    //     const spy = sinon.spy(instance, 'changePublishState')
-    //     wrapper.find(Button).at(2).simulate('click')
-    //     sinon.assert.called(spy)
-    // })
+    test('changePublishState click', () => {
+        const wrapper = mount(<Versions {...props} />)
+        const instance = wrapper.instance()
+        wrapper.setState({ 'login': true })
+        wrapper.setState({
+            'results': [[{ "type": "draft", "icon": "BlankImage", "path": "/modules/test", "version": "Version 1", "publishedState": 'Not published', "updatedDate": "", "firstButtonType": 'primary', "secondButtonType": 'secondary', "firstButtonText": 'Publish', "secondButtonText": 'Preview', "isDropdownOpen": false, "isArchiveDropDownOpen": false, "metadata": '' }]],
+        })
+        const spy = sinon.spy(instance, 'changePublishState')
+        wrapper.find(Button).at(2).simulate('click')
+        sinon.assert.called(spy)
+    })
 
-    // it('test fetchProducts function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     const spy = sinon.spy(inst, 'fetchProducts')
-    //     inst.componentDidMount()
-    //     sinon.assert.called(spy)
-    // })
+    it('test fetchProducts function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        const spy = sinon.spy(inst, 'fetchProducts')
+        inst.componentDidMount()
+        sinon.assert.called(spy)
+    })
 
-    // it('test fetchVersions function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     const spy = sinon.spy(inst, 'fetchVersions')
-    //     inst.fetchVersions()
-    //     sinon.assert.called(spy)
-    // })
+    it('test fetchVersions function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        const spy = sinon.spy(inst, 'fetchVersions')
+        inst.fetchVersions()
+        sinon.assert.called(spy)
+    })
 
-    // it('test changePublishState function', () => {
-    //     const wrapper = renderer.create(<Versions {...props} />)
-    //     const inst = wrapper.getInstance()
-    //     const spy = sinon.spy(inst, 'changePublishState')
-    //     inst.changePublishState("ds")
-    //     sinon.assert.called(spy)
-    // })
+    it('test changePublishState function', () => {
+        const wrapper = renderer.create(<Versions {...props} />)
+        const inst = wrapper.getInstance()
+        const spy = sinon.spy(inst, 'changePublishState')
+        inst.changePublishState("ds")
+        sinon.assert.called(spy)
+    })
 })
