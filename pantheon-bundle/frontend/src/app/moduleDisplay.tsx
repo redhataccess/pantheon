eed25b49956dfb44c2eae6ec46a365c918c5a7b9
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2020-10-20 18:41:32 +0530

    CCS-3812: Module details page needs to be refreshed for the "View on portal" link to show-up. Updating for the changes in master. Reverting the commit (#440)

--- pantheon-bundle/frontend/src/app/moduleDisplay.tsx
+++ pantheon-bundle/frontend/src/app/moduleDisplay.tsx
@@ -302,10 +302,10 @@ class ModuleDisplay extends Component<any, IModuleDisplayState> {
 
     private onPublishEvent = () => {
         this.getVersionUUID(this.props.location.pathname)
-        // this is a dirty fix since useEffect hook cannot be used with class based components
-        while (this.state.portalUrl===""){
+
+        setTimeout(()=> {
             this.getPortalUrl(this.props.location.pathname.substring(PathPrefixes.MODULE_PATH_PREFIX.length), this.state.variant)
-        }
+        }, 500)
     }
 
     private getVersionUUID = (path) => {
