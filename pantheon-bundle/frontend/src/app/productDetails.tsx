8c6ff2c96e16be5b2fa50ba877e886d39115a53e
Sydney Grant <sgrant@redhat.com>
2020-11-25 11:44:40 -0500

    CCS-3510-back-button-fix

--- pantheon-bundle/frontend/src/app/productDetails.tsx
+++ pantheon-bundle/frontend/src/app/productDetails.tsx
@@ -1,224 +1,64 @@
-import React, { Component } from "react"
+import React, { useState, useEffect, useContext } from "react"
 import {
     ActionGroup, Alert, AlertActionCloseButton, Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Level, LevelItem, List, ListItem,
     Text, TextContent, TextVariants, TextInput, InputGroup
 } from "@patternfly/react-core"
 import { Fields, JcrTypes, SlingTypes } from "@app/Constants"
-import { tsObjectKeyword } from "@babel/types";
+import { ProductContext, IProduct } from "@app/contexts/ProductContext"
 
 export interface IProps {
     productName: string
 }
 
-interface IState {
-    allVersionNames: any[]
-    failedPost: boolean
-    isMissingFields: boolean
-    isUrlFragmentValid: boolean
-    newVersion: string
-    urlFragment: string
-}
+export const ProductDetails = (props: any) => {
+    const allProducts = useContext(ProductContext);
 
-class ProductDetails extends Component<IProps, IState> {
-    public versionNames: string[] = []
+    const [failedPost, setFailedPost] = useState(false)
+    const [isMissingFields, setIsMissingFields] = useState(false)
+    const [urlFragment, setUrlFragment] = useState("")
+    const [isUrlFragmentValid, setIsUrlFragmentValid] = useState(true)
+    const [newVersion, setNewVersion] = useState("")
+    const [allVersionNames, setAllVersionNames] = useState([] as string[])
+    const [currentProduct, setCurrentProduct] = useState(undefined as IProduct | undefined)
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            allVersionNames: [],
-            failedPost: false,
-            isMissingFields: false,
-            isUrlFragmentValid: true,
-            newVersion: "",
-            urlFragment: "",
+    useEffect(() => {
+        if (allProducts) {
+            const currentProduct = allProducts.find(a => a["jcr:uuid"] === props.match.params.id)
+            setCurrentProduct(currentProduct)
+            fetchProductVersions(currentProduct)
         }
-    }
+    }, [allProducts])
 
-    public componentWillReceiveProps(nextProps) {
-        // allow page load from productDetails to products listing
-        if (nextProps.productName !== undefined && nextProps.productName.trim() !== "") {
-            return window.location.reload(false)
-        }
-    }
-
-    public componentDidMount() {
-        this.fetchProductDetails(this.state.allVersionNames)
-    }
-
-    public render() {
-        return (
-            <React.Fragment>
-                <div className="app-container">
-                    <Breadcrumb>
-                        <BreadcrumbItem ><a href="#/products" onClick={() => window.location.reload(false)}>All Products</a></BreadcrumbItem>
-                        <BreadcrumbItem to="#" isActive={true}>Product Details</BreadcrumbItem>
-                    </Breadcrumb>
-                </div>
-                <div className="app-container">
-                    <Level>
-                        <LevelItem>
-                            <TextContent>
-                                <Text component={TextVariants.h1}>{this.props.productName}{"  "}</Text>
-                            </TextContent>
-                        </LevelItem>
-                        <LevelItem />
-                    </Level>
-                </div>
-                {this.state.allVersionNames.length !== 0 && (
-                    <div className="app-container">
-                        Versions:
-                        <List>
-                            {this.state.allVersionNames.map((version) =>
-                                <ListItem key={version}>{version}</ListItem>
-                            )}
-                        </List>
-                    </div>)}
-                <br />
-                <div>
-                    <Form>
-                        <div className="pant-form-width-md">
-                            {this.state.isMissingFields &&
-                                <div className="notification-container">
-                                    <Alert variant="warning"
-                                        title="Fields indicated by * are mandatory"
-                                        actionClose={<AlertActionCloseButton onClose={this.dismissNotification} />}
-                                    />
-                                </div>
-                            }
-                            {!this.state.isUrlFragmentValid &&
-                                <div className="notification-container">
-                                    <Alert variant="warning"
-                                        title="Allowed input for ulrFragment: alphanumeric, hyphen, period and underscore"
-                                        actionClose={<AlertActionCloseButton onClose={this.dismissNotification} />}
-                                    />
-                                </div>
-                            }
-                            <FormGroup
-                                label="New Version:"
-                                isRequired={true}
-                                fieldId="new_version_name"
-                            >
-                                <TextInput id="new_version_name_text" type="text" placeholder="New version name" onChange={this.handleTextInputChange} value={this.state.newVersion} />
-                            </FormGroup>
-                            <br />
-                            <FormGroup
-                                label=" URL Fragment:"
-                                isRequired={true}
-                                fieldId="url_fragment"
-                            >
-                                <TextInput id="url_fragment_text" type="text" placeholder="Url fragment" onChange={this.handleUrlInputChange} value={this.state.urlFragment} />
-                            </FormGroup>
-                            {this.state.failedPost &&
-                                <div className="notification-container">
-                                    <Alert
-                                        variant="danger"
-                                        title="Failed to create product version."
-                                        actionClose={<AlertActionCloseButton onClose={this.dismissNotification} />}
-                                    >
-                                        Please check if you are logged in as a publisher.
-                                    </Alert>
-                                </div>
-                            }
-                            <ActionGroup>
-                                <Button aria-label="Creates a new Version Name." onClick={this.saveVersion}>Save</Button>
-                            </ActionGroup>
-                        </div>
-                    </Form>
-                </div>
-            </React.Fragment>
-        )
-    }
-
-    private dismissNotification = () => {
-        this.setState({ failedPost: false, isMissingFields: false })
-    }
-
-    private fetchProductDetails = (versionNames) => {
-        // setup url fragment
-        const urlFragment = this.props.productName.toString().toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
+    const fetchProductVersions = (currentProduct) => {
+        const urlFragment = currentProduct && currentProduct.name.toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
         const path = "/content/products/" + urlFragment + "/versions.2.json"
-        let key
-        versionNames = []
-
-        fetch(path)
-            .then((response) => {
-                if (response.ok) {
-                    return response.json()
-                } else if (response.status === 404) {
-                    // create versions path
-                    this.createVersionsPath()
-                    return versionNames
-                } else {
-                    throw new Error(response.statusText)
+        let versionNames: string[] = []
+        fetch(path).then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else if (response.status === 404) {
+                // create versions path
+                createVersionsPath(urlFragment)
+                return versionNames
+            } else {
+                throw new Error(response.statusText)
+            }
+        }).then(json => {
+            for (let i = 0; i < Object.keys(json).length; i++) {
+                const key = Object.keys(json)[i]
+                if (key !== Fields.JCR_PRIMARYTYPE && json[key].name !== undefined) {
+                    versionNames.push(json[key].name)
                 }
-            })
-            .then(responseJSON => {
-                // tslint:disable-next-line: prefer-for-of
-                for (let i = 0; i < Object.keys(responseJSON).length; i++) {
-                    key = Object.keys(responseJSON)[i]
-                    if ((key !== Fields.JCR_PRIMARYTYPE)) {
-                        if (responseJSON[key].name !== undefined) {
-                            versionNames.push(responseJSON[key].name)
-                        }
-                    }
-                }
-                this.setState({
-                    allVersionNames: versionNames
-                })
-            })
+            }
+            setAllVersionNames(versionNames)
+        })
             .catch((error) => {
                 console.log(error)
             })
-        return versionNames
     }
 
-    private handleTextInputChange = newVersion => {
-        this.setState({ newVersion })
-    }
-
-    private handleUrlInputChange = urlFragment => {
-        if (/^[-.\w]+$/.test(urlFragment)) {
-            this.setState({ urlFragment, isUrlFragmentValid: true })
-        } else {
-            this.setState({ isUrlFragmentValid: false})
-        }
-    }
-
-    private saveVersion = () => {
-        // confirming form state is valid
-        if (this.state.newVersion === "" || this.state.urlFragment === "") {
-            this.setState({ isMissingFields: true })
-        } else {
-            // creating formData obj and adding form's data to it
-            const formData = new FormData()
-            formData.append(Fields.NAME, this.state.newVersion)
-            formData.append(Fields.SLING_RESOURCETYPE, SlingTypes.PRODUCT_VERSION)
-            formData.append(Fields.JCR_PRIMARYTYPE, JcrTypes.PRODUCT_VERSION)
-            formData.append(Fields.URL_FRAGMENT, this.state.urlFragment)
-            
-            // creating url-friendly version of product name and product version
-            const productUrlFragment = this.props.productName.toString().toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
-            const encodedVersion = this.state.newVersion.toString().toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
-            // sending formData to backend
-            fetch(encodeURI("/content/products/" + productUrlFragment + "/versions/" + encodedVersion), {
-                body: formData,
-                method: "post",
-            }).then(response => {
-                if (response.status === 200 || response.status === 201) {
-                    this.setState({ newVersion: "", urlFragment: "" })
-                    this.fetchProductDetails(this.state.allVersionNames)
-                } else {
-                    this.setState({ failedPost: true, isMissingFields: false })
-                    console.log("Version adding failure")
-                }
-            })
-        }
-
-    }
-
-    private createVersionsPath = () => {
+    const createVersionsPath = (urlFragment) => {
         const formData = new FormData()
-        const urlFragment = this.props.productName.toString().toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
         fetch(encodeURI("/content/products/" + urlFragment + "/versions"), {
             body: formData,
             method: "post",
@@ -226,12 +66,142 @@ class ProductDetails extends Component<IProps, IState> {
             if (response.status === 200 || response.status === 201) {
                 console.log(" Created versions path " + response.status)
             } else {
-                console.log(" Created versions path  failed!")
-
+                console.log(" Created versions path failed!")
             }
         })
     }
 
-}
+    const dismissNotification = () => {
+        setFailedPost(false)
+        setIsMissingFields(false)
+    }
 
-export { ProductDetails }
+    const handleTextInputChange = newVersion => {
+        setNewVersion(newVersion)
+    }
+
+    const handleUrlInputChange = urlFragment => {
+        if (/^[-.\w]+$/.test(urlFragment)) {
+            setUrlFragment(urlFragment)
+            setIsUrlFragmentValid(true)
+        } else {
+            setIsUrlFragmentValid(false)
+        }
+    }
+
+    const saveVersion = () => {
+        if (newVersion === "" || urlFragment === "") {
+            setIsMissingFields(true)
+        } else {
+            // creating formData obj and adding form's data to it
+            const formData = new FormData()
+            formData.append(Fields.NAME, newVersion)
+            formData.append(Fields.SLING_RESOURCETYPE, SlingTypes.PRODUCT_VERSION)
+            formData.append(Fields.JCR_PRIMARYTYPE, JcrTypes.PRODUCT_VERSION)
+            formData.append(Fields.URL_FRAGMENT, urlFragment)
+
+            // creating url-friendly version of product name and product version
+            const productUrlFragment = currentProduct && currentProduct.name.toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
+            const encodedVersion = newVersion.toString().toLowerCase().replace(/[^A-Z0-9]+/ig, "_")
+            // sending formData to backend
+            fetch(encodeURI("/content/products/" + productUrlFragment + "/versions/" + encodedVersion), {
+                body: formData,
+                method: "post",
+            }).then(response => {
+                if (response.status === 200 || response.status === 201) {
+                    setNewVersion("")
+                    setUrlFragment("")
+                    currentProduct && fetchProductVersions(currentProduct)
+                } else {
+                    setFailedPost(true)
+                    setIsMissingFields(false)
+                    console.log("Version adding failure")
+                }
+            })
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <div className="app-container">
+                <Breadcrumb>
+                    <BreadcrumbItem ><a href="#/products">All Products</a></BreadcrumbItem>
+                    <BreadcrumbItem to="#" isActive={true}>Product Details</BreadcrumbItem>
+                </Breadcrumb>
+            </div>
+            {currentProduct && <div className="app-container">
+                <Level>
+                    <LevelItem>
+                        <TextContent>
+                            <Text component={TextVariants.h1}>{currentProduct.name}{"  "}</Text>
+                        </TextContent>
+                    </LevelItem>
+                    <LevelItem />
+                </Level>
+            </div>}
+            {allVersionNames.length !== 0 && (
+                <div className="app-container">
+                    Versions:
+                    <List>
+                        {allVersionNames.map((version) =>
+                            <ListItem key={version}>{version}</ListItem>
+                        )}
+                    </List>
+                </div>)}
+            <br />
+            <div>
+                <Form>
+                    <div className="pant-form-width-md">
+                        {isMissingFields &&
+                            <div className="notification-container">
+                                <Alert variant="warning"
+                                    title="Fields indicated by * are mandatory"
+                                    actionClose={<AlertActionCloseButton onClose={dismissNotification} />}
+                                />
+                            </div>
+                        }
+                        {!isUrlFragmentValid &&
+                            <div className="notification-container">
+                                <Alert variant="warning"
+                                    title="Allowed input for ulrFragment: alphanumeric, hyphen, period and underscore"
+                                    actionClose={<AlertActionCloseButton onClose={dismissNotification} />}
+                                />
+                            </div>
+                        }
+                        <FormGroup
+                            label="New Version:"
+                            isRequired={true}
+                            fieldId="new_version_name"
+                        >
+                            <TextInput id="new_version_name_text" type="text" placeholder="New version name" onChange={handleTextInputChange} value={newVersion} />
+                        </FormGroup>
+                        <br />
+                        <FormGroup
+                            label=" URL Fragment:"
+                            isRequired={true}
+                            fieldId="url_fragment"
+                        >
+                            <TextInput id="url_fragment_text" type="text" placeholder="Url fragment" onChange={handleUrlInputChange} value={urlFragment} />
+                        </FormGroup>
+                        {failedPost &&
+                            <div className="notification-container">
+                                <Alert
+                                    variant="danger"
+                                    title="Failed to create product version."
+                                    actionClose={<AlertActionCloseButton onClose={dismissNotification} />}
+                                >
+                                    Please check if you are logged in as a publisher.
+                                    </Alert>
+                            </div>
+                        }
+                        {allProducts && allProducts.length > 0 &&
+                            <ActionGroup>
+                                <Button aria-label="Creates a new Version Name." onClick={saveVersion}>Save</Button>
+                            </ActionGroup>
+                        }
+                    </div>
+                </Form>
+            </div>
+        </React.Fragment>
+    )
+}
--- pantheon-bundle/frontend/src/app/productListing.tsx
+++ pantheon-bundle/frontend/src/app/productListing.tsx
@@ -4,109 +4,74 @@ import {
   OptionsMenu, OptionsMenuItem, OptionsMenuToggle, TextInput
 } from "@patternfly/react-core";
 import "@app/app.css";
-import { ProductDetails } from "@app/productDetails";
 import { CaretDownIcon } from "@patternfly/react-icons";
-import { ProductProvider, ProductContext } from "@app/contexts/ProductContext"
-
-const onSelect = (id) => () => {
-  //@ts-ignore
-  window.location += `/${id}`;
-};
-
-// private onSelect = (event, data) => () => {
-//   this.setState({
-//     isProductDetails: !this.state.isProductDetails,
-//     productName: data.name,
-//   });
-// };
-// let data: any = [];
+import { ProductContext, IProduct } from "@app/contexts/ProductContext"
 
 export default function ProductListing(props: any) {
+  const allProducts = useContext(ProductContext);
+  
   const [input, setInputField] = useState("")
-  const [results, setResults] = useState([])
-  const [data, setData]: any = useState([]);
-  let temp: any = useContext(ProductContext);
- 
-  
+  const [filteredProducts, setFilteredProducts] = useState([] as IProduct[])
+
   useEffect(() => {
-    // create dynamic isOpen field for each result
-    let data: any = [];
-    setData(data);
-  }, [temp]);
-  
+    setFilteredProducts(allProducts)
+  }, [allProducts])
 
-
-  
-
-  const onToggle = (id) => (event: any) => {
-    let newState = data;
-    //@ts-ignore
-  //  data.map(i => {
-  //     if (i["jcr:uuid"] === id) {
-  //       console.log("data before click:", i.isOpen);
-  //       i.isOpen = !i.isOpen;
-  //       console.log("data after click:", i.isOpen);
-  //     }
-  //   });
-    newState.forEach(product => {
-      if(product["jcr:uuid"] === id){
-        product.isOpen = !product.isOpen;
-        setData(newState);
-      }
-    })
-    // console.log("data after click", data);
-   
+  const onToggle = (id) => {
+    updateProduct(id);
   };
 
-  const setInput = input => {
-    const versions: string[] = [];
-    let searchString = "";
-    setInputField(input)
-    //@ts-ignore
+  const onSelect = (id) => {
+    // @ts-ignore
+    window.location += `/${id}`;
+  };
 
-    data.map(data => {
-      searchString = "" + data.name
-      if (searchString.toLowerCase().includes(input.toLowerCase())) {
-        versions.push(data)
-      }
+  const setInput = (text, event) => {
+    setInputField(text)
+    const filteredProducts = allProducts.filter(product => {
+      return product.name.toLowerCase().includes(text.toLowerCase())
     });
-    //@ts-ignore
-    setResults(versions)
+    setFilteredProducts(filteredProducts)
   }
 
-  // let input;
+  function updateProduct(id: string) {
+    const newProducts = filteredProducts.map(product => {
+      if (product['jcr:uuid'] === id) {
+        product.isOpen = !product.isOpen;
+      }
+      return product;
+    });
+    setFilteredProducts(newProducts);
+  }
 
   return (
-      <>
-
-        <FormGroup
-          label="Search Products"
-          fieldId="search"
-        >
-          <div className="row-view">
-            <TextInput id="search" type="text" onChange={setInput} placeholder="Type product name to search" value={input} />
-          </div>
-        </FormGroup>
-        <DataList aria-label="single action data list example ">
-          {false && (
-            <DataListItem aria-labelledby="single-action-item1">
-              <DataListItemRow>
-                <DataListItemCells
-                  dataListCells={[
-                    <DataListCell key="primary content">
-                      <span className="sp-prop-nosort" id="product-name">Product Name</span>
-                    </DataListCell>,
-                    <DataListCell key="secondary content" width={2}>
-                      <span className="sp-prop-nosort" id="product-description">Product Description</span>
-                    </DataListCell>
-                  ]}
-                />
-              </DataListItemRow>
-            </DataListItem>
-          )}
-
-        {data.length &&
-          data.map((product, key) => {
+    <>
+      <FormGroup
+        label="Search Products"
+        fieldId="search">
+        <div className="row-view">
+          <TextInput id="search" type="text" onChange={(text, event) => setInput(text, event)} placeholder="Type product name to search" value={input} />
+        </div>
+      </FormGroup>
+      <DataList aria-label="single action data list example ">
+        {(filteredProducts && filteredProducts.length > 0) && (
+          <DataListItem aria-labelledby="single-action-item1">
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="primary content">
+                    <span className="sp-prop-nosort" id="product-name">Product Name</span>
+                  </DataListCell>,
+                  <DataListCell key="secondary content" width={2}>
+                    <span className="sp-prop-nosort" id="product-description">Product Description</span>
+                  </DataListCell>
+                ]}
+              />
+            </DataListItemRow>
+          </DataListItem>
+        )}
+        {(filteredProducts && filteredProducts.length > 0) &&
+          filteredProducts.map((product, key) => {
             return <DataListItem key={key} aria-labelledby="multi-actions-item1">
               <DataListItemRow>
                 <DataListItemCells key={product["jcr:uuid"]}
@@ -117,18 +82,17 @@ export default function ProductListing(props: any) {
                     <DataListCell key="secondary content" width={2}>{product.description}</DataListCell>,
                     <DataListCell key="Dropdown content">
                       <DataListAction
-                        aria-labelledby="multi-actions-item1 {data['jcr:uuid']}"
-                        id="{data['jcr:uuid']}"
+                        aria-labelledby={`multi-actions-item1 ${product['jcr:uuid']}`}
+                        id={`${product['jcr:uuid']}`}
                         aria-label="Actions"
                       >
                         <OptionsMenu
                           isPlain={true}
-                          id={data["jcr:uuid"]}
+                          id={`product-${product["jcr:uuid"]}`}
                           menuItems={[
-                            <OptionsMenuItem key="dropdown">Product Details</OptionsMenuItem>]}
-                          isOpen={key.isOpen}
-                          // @ts-ignore
-                          toggle={<OptionsMenuToggle onToggle={onToggle(product["jcr:uuid"])} toggleTemplate={<CaretDownIcon aria-hidden="true" />} aria-label="Sort by" hideCaret={true} />} />
+                            <OptionsMenuItem onSelect={() => onSelect(product["jcr:uuid"])} key="dropdown">Product Details</OptionsMenuItem>]}
+                          isOpen={product.isOpen}
+                          toggle={<OptionsMenuToggle onToggle={() => onToggle(product["jcr:uuid"])} toggleTemplate={<CaretDownIcon aria-hidden="true" />} aria-label="Sort by" hideCaret={true} id={`product-${product["jcr:uuid"]}-button`} />} />
                       </DataListAction>
                     </DataListCell>
                   ]}
@@ -136,20 +100,20 @@ export default function ProductListing(props: any) {
               </DataListItemRow>
             </DataListItem>
           })}
-          {!data.length && 
-            <DataListItem aria-labelledby="single-action-item0" data-testid="emptyResults">
-              <DataListItemRow>
-                <DataListItemCells
-                  dataListCells={[
-                    <DataListCell key="primary content" width={2}>
-                      <span className="sp-prop-nosort" id="product-name">No products found</span>
-                    </DataListCell>
-                  ]}
-                />
-              </DataListItemRow>
-            </DataListItem>
-          }
-        </DataList>
+        {(!(filteredProducts && filteredProducts.length > 0)) &&
+          <DataListItem aria-labelledby="single-action-item0" data-testid="emptyResults">
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="primary content" width={2}>
+                    <span className="sp-prop-nosort" id="product-name">No products found</span>
+                  </DataListCell>
+                ]}
+              />
+            </DataListItemRow>
+          </DataListItem>
+        }
+      </DataList>
     </>
-);
+  );
 }
