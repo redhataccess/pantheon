a7f714df81a223a273357fee1d1207f7f1c6ca1d
Sydney Grant <sgrant@redhat.com>
2021-04-15 12:35:31 -0400

    CCS-3982-implement-error-boundary

---
+++ pantheon-bundle/frontend/src/app/ErrorBoundary.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import "@app/app.css";
+import { Alert } from '@patternfly/react-core';
+
+export interface IErrorBoundaryProps {
+    hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<IErrorBoundaryProps, any> {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    // Log error
+    console.log(error);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Render fallback UI
+      return(
+        <React.Fragment>
+     
+        <Alert isInline variant="danger" title="An Error Has Occurred.">
+          <p>
+            Please click{' '}
+            <a href="/">here</a>
+           {' '} to return to the homepage.
+          </p>
+        </Alert>
+       
+      </React.Fragment>
+      )
+    }
+
+    return this.props.children; 
+  }
+}
+
+export { ErrorBoundary }; 
+
