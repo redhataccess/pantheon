ba004f14e232a47f586531ad447ae60b04145dae
Lisa Davidson <llu@redhat.com>
2021-04-22 12:19:06 -0400

    refactor XrefValidtionHelper to remove all the static keywords

--- pantheon-bundle/src/main/java/com/redhat/pantheon/validation/helper/XrefValidationHelper.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/validation/helper/XrefValidationHelper.java
@@ -8,21 +8,21 @@ import java.util.Map;
 
 public class XrefValidationHelper {
 
-    private static List<String> xRefs;
+    private List<String> xRefs;
 
     public XrefValidationHelper() {
         initList();
     }
 
-    public static List<String> getObjectsToValidate() {
+    public List<String> getObjectsToValidate() {
         return xRefs;
     }
 
-    public static void initList() {
+    public void initList() {
         xRefs = new ArrayList<>();
     }
 
-    public static void setObjectsToValidate(List<String> objectsToValidate) {
+    public void setObjectsToValidate(List<String> objectsToValidate) {
         if(objectsToValidate.size()>0){
             xRefs.addAll(objectsToValidate);
         }
