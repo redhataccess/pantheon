21f5a94ff18653488a24c01ca2e5909a7f0bb333
Sydney Grant <sgrant@redhat.com>
2021-05-10 17:03:46 -0400

    completed git import messaging

--- pantheon-bundle/frontend/src/app/contexts/GitImportContext.tsx
+++ pantheon-bundle/frontend/src/app/contexts/GitImportContext.tsx
@@ -44,6 +44,7 @@ export function GitImportProvider({ children }: IGitImportProviderProps) {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
   };
+  let git2pantheonURL
   let statusKey;
   let currRepository;
 
@@ -54,8 +55,35 @@ export function GitImportProvider({ children }: IGitImportProviderProps) {
       'branch': branch,
       'repo': repository
     }
+    fetch("/conf/pantheon/pant:syncServiceUrl")
+    .then((resp => {
+      if (!resp.ok) {
+        console.log('Error occurred, could not find the git2pantheon URL configuration.');
+        const singleUpload = {
+          id: Date.now(),
+          totalFiles: 0,
+          repoName: repository,
+          success: false
+        };
+        setUploads([...uploads, singleUpload]);
+      } else {
+        resp.text().then((text) => {
+          git2pantheonURL= text
+          console.log("The response text from pant:syncServiceUrl is: " + text)
+        })
+        .then(() => {
+          if (git2pantheonURL === "") {
+            console.log('Error occurred, the git2pantheon URL configuration is blank.')
+            const singleUpload = {
+              id: Date.now(),
+              totalFiles: 0,
+              repoName: repository,
+              success: false
+            };
+            setUploads([...uploads, singleUpload]);
+          } else {
     //first fetch call will return a status key as a response if successful 
-    fetch('http://127.0.0.1:5000/api/clone', {
+    fetch(git2pantheonURL + "/api/clone", {
       body: JSON.stringify(body),
       headers: hdrs,
       method: 'POST'
@@ -79,7 +107,7 @@ export function GitImportProvider({ children }: IGitImportProviderProps) {
         //second fetch takes the status key and returns the status of repo upload once it is complete
         //we keep making this call until status === "done" signifying the upload has completed
         const getStatus = async () => {
-          return await fetch('http://127.0.0.1:5000/api/status', {
+          return await fetch(git2pantheonURL + "/api/status", {
 
             body: statusKey,
             headers: hdrs,
@@ -103,6 +131,10 @@ export function GitImportProvider({ children }: IGitImportProviderProps) {
         });
       }
     })).catch(e => console.error("ERROR! in /status", e));
+  }
+        })
+        }
+      }))
   }
 
   // components wrapped in this provider will now have access to uploads and cloneRepoGit2Pantheon
