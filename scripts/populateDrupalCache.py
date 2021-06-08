e940d2c83181565209089abf6ff25e1e3ce039b2
Ben Radey <benjamin.radey+gpg@gmail.com>
2021-06-08 15:04:02 -0400

    Code review feedback

--- scripts/populateDrupalCache.py
+++ scripts/populateDrupalCache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import requests
 from xml.dom import minidom
@@ -8,25 +8,42 @@ sitemaps = [
     'https://access.redhat.com/sitemap/module.sitemap.xml'
 ]
 
-success = 0
-failure = 0
+def gather_urls():
+    urls = []
+    for sitemap in sitemaps:
+        r = requests.get(sitemap)
+        if r.status_code != 200:
+            print('Error, status code for ' + sitemap + ' was ' + r.status_code)
+        else:
+            xmldoc = minidom.parseString(r.content)
+            itemlist = xmldoc.getElementsByTagName('loc')
+            for item in itemlist:
+                urls.append(item.firstChild.data)
+    return urls
 
-for sitemap in sitemaps:
-    r = requests.get(sitemap)
 
-    if r.status_code != 200:
-        print('Error, status code for ' + url + ' was ' + r.status_code)
-    else:
-        xmldoc = minidom.parseString(r.content)
-        itemlist = xmldoc.getElementsByTagName('loc')
-        print('\nProcessing ' + str(len(itemlist)) + ' items from ' + sitemap + '\n')
-        for item in itemlist:
-            try:
-                rr = requests.get(item.firstChild.data)
-                print(str(rr.status_code) + ': ' + rr.url)
-                success += 1
-            except requests.exceptions.RequestException as e:
-                print(e)
-                failure += 1
+def perform_requests(urls):
+    success = 0
+    failure = 0
+    for url in urls:
+        try:
+            rr = requests.get(url)
+            print(str(rr.status_code) + ': ' + rr.url)
+            success += 1
+        except requests.exceptions.RequestException as e:
+            print(e)
+            failure += 1
+    return [success, failure]
 
-print('\nMade ' + str(success) + ' requests and raised ' + str(failure) + ' errors.')
+
+def main():
+    urls = gather_urls()
+    q = perform_requests(urls)
+    successes = q[0]
+    failures = q[1]
+
+    print('\nMade ' + str(successes) + ' requests and raised ' + str(failures) + ' errors.')
+
+
+if __name__ == '__main__':
+    main()
