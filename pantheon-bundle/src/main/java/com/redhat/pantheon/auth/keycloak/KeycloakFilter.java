0c231cef45c12fcbbe8e6622b1a11a75084be7be
Lisa Davidson <llu@redhat.com>
2021-07-14 17:00:04 -0400

    redirect users to sso on starter.html

--- pantheon-bundle/src/main/java/com/redhat/pantheon/auth/keycloak/KeycloakFilter.java
+++ pantheon-bundle/src/main/java/com/redhat/pantheon/auth/keycloak/KeycloakFilter.java
@@ -32,7 +32,7 @@ import org.slf4j.LoggerFactory;
         service = Filter.class,
         property = {
                 KeycloakOIDCFilter.CONFIG_FILE_PARAM + "=" + "keycloak.json",
-                "keycloak.config.skipPattern=(/pantheon/internal/modules.json|/pantheon/builddate.json|/pantheon/fonts/*|/content/repositories.harray.1.json|/starter.html|/bin/browser.html|/content/starter/css/bundle.css|/content/starter/img/sling-logo.svg|/content/starter/img/asf-logo.svg|/content/starter/img/sling-logo.svg|/content/starter/img/gradient.jpg|/content/starter/fonts/OpenSans-Light-webfont.woff|/content/starter/fonts/OpenSans-Regular-webfont.woff|/system/sling.js|/system/*|/pantheon/*.js|/static/rhdocs.css|/pantheon/fonts/RedHatText-Medium.woff|/conf/pantheon/*)",
+                "keycloak.config.skipPattern=(/pantheon/internal/modules.json|/pantheon/builddate.json|/pantheon/fonts/*|/content/repositories.harray.1.json|/bin/browser.html|/content/starter/css/bundle.css|/content/starter/img/sling-logo.svg|/content/starter/img/asf-logo.svg|/content/starter/img/sling-logo.svg|/content/starter/img/gradient.jpg|/content/starter/fonts/OpenSans-Light-webfont.woff|/content/starter/fonts/OpenSans-Regular-webfont.woff|/system/sling.js|/system/*|/pantheon/*.js|/static/rhdocs.css|/pantheon/fonts/RedHatText-Medium.woff|/conf/pantheon/*)",
                 HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN + "=" + "/pantheon/*",
                 HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN + "=" + "/content/pantheon",
                 HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN + "=" + "/content/products",
